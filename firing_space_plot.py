3660653bade647a396edacb3927c4c8d86921474
abuzarmahmood <abuzarmahmood@gmail.com>
2018-07-03 10:26:13 -0500

    First commit

---
+++ firing_space_plot.py
@@ -0,0 +1,6 @@
+# 1) Open single file for now
+# 2) extract spiking for all tastes for on&off conditions
+# 3) Compute firing rate for ENTIRE 7s (or whatever length of recording)
+# 3.5) Maybe smooth firing rate
+# 4) Project into 'n' dim space and reduce dimensions
+
