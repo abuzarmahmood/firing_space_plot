e2190ce239916bdc776f07e0afabc798e7c582ae
abuzarmahmood <abuzarmahmood@gmail.com>
2018-07-03 18:59:32 -0500

    Refining

--- firing_space_plot.py
+++ firing_space_plot.py
@@ -4,6 +4,8 @@
 # 3.5) Maybe smooth firing rate
 # 4) Project into 'n' dim space and reduce dimensions
 
+# Normalize firing rate, use only responsive neurons (probs not needed)
+# Try projecting all tastes at same time
 ######################### Import dat ish #########################
 import tables
 import easygui
@@ -60,7 +62,7 @@ tot_time = 7000
 
 off_firing = []
 
-for l in range(len(off_spikes)):
+for l in range(len(off_spikes)): # How TF do you get nan's from means?
     # [trials, nrns, time]
     this_off_firing = np.zeros((off_spikes[0].shape[0],off_spikes[0].shape[1],int((tot_time-window_size)/step_size)-1))
     for i in range(this_off_firing.shape[0]):
@@ -69,7 +71,17 @@ for l in range(len(off_spikes)):
                 this_off_firing[i, j, k] = np.mean(off_spikes[l][i, j, step_size*k:step_size*k + window_size])
     this_off_firing = this_off_firing.reshape((this_off_firing.shape[1],this_off_firing.shape[0]*this_off_firing.shape[2]))
     off_firing.append(this_off_firing)
+    
+# Normalize firing
+for l in range(len(off_firing)):
+    for m in range(off_firing[0].shape[0]):
+        min_val = np.min(off_firing[l][m,:])
+        max_val = np.max(off_firing[l][m,:])
+        off_firing[l][m,:] = (off_firing[l][m,:] - min_val)/(max_val-min_val)
 
+all_off_f = np.asarray(off_firing)
+all_off_f = all_off_f.reshape((all_off_f.shape[1],all_off_f.shape[0]*all_off_f.shape[2]))
+#all_off_f = all_off_f[~np.isnan(all_off_f)]
 ## Make sure there are not problems with reshaping    
 ## Maybe smooth firing rates
 
@@ -84,18 +96,40 @@ for l in range(len(off_spikes)):
 # plt.plot(rate2)
 # =============================================================================
 
+## All da neurons, all da tastes, and ALL DA PLOTS!
+# =============================================================================
+# rows = len(train_rate)
+# rows = len(train_rate)
+# cols = train_rate[0].shape[1]
+# count = 1
+# x = np.linspace(0,10,100)
+# fig, axes = plt.subplots(rows,cols,sharex = 'all',sharey='all')
+# for i in range(rows):
+#     for j in range(cols):
+#         axes[i,j].hist(np.ndarray.flatten(train_rate[i][:,j,:]),density=1)
+#         params = train_params[i][j]
+#         #y = pdf_plot(x,params)
+#         #axes[i,j].plot(x,y,colors[int(params[2])])
+#         count +=1
+#         print(count)
+#    
+# fig.tight_layout()
+# =============================================================================
+
 ################### Reduce dimensions ########################
-off_f_red = LLE(n_neighbors = 50,n_components=3).fit_transform(np.transpose(this_off_firing))
+off_f_red = LLE(n_neighbors = 50,n_components=3).fit_transform(np.transpose(all_off_f))
+off_f_red = LLE(n_neighbors = 50,n_components=3).fit_transform(np.transpose(off_firing[0]))
 off_f_red = TSNE(n_components=3).fit_transform(np.transpose(this_off_firing))
 
 ## 3D Plot for single trajectory
 fig = plt.figure()
 ax = Axes3D(fig)
-i = 1
+i = 2
 trial_len = int((tot_time-window_size)/step_size)-1
 ran_inds = np.arange((trial_len*i),(trial_len*(i+1)))
 this_cmap = Colormap('hsv')
-p = ax.scatter(off_f_red[ran_inds,0],off_f_red[ran_inds,1],off_f_red[ran_inds,2],c =np.linspace(1,255,len(ran_inds)),cmap='prism')
+p = ax.scatter(off_f_red[ran_inds,0],off_f_red[ran_inds,1],off_f_red[ran_inds,2],
+               c =np.linspace(1,255,len(ran_inds)),cmap='hsv')
 ax.plot(off_f_red[ran_inds,0],off_f_red[ran_inds,1],off_f_red[ran_inds,2])
 fig.colorbar(p)
 
