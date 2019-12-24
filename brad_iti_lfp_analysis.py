c8d98961b46f58305e42df0ad1fd9562407ac104
abuzarmahmood <abuzarmahmood@gmail.com>
2019-12-24 14:14:52 -0500

    brad_iti_lfp_analysis: Added comparison of bandpassed amplitude from hilbert transform

--- brad_iti_lfp_analysis.py
+++ brad_iti_lfp_analysis.py
@@ -104,6 +104,7 @@ plt.imshow(normalized_whole_firing,
 #
 
 # Extract whole session LFPs
+# This has already been bandpassed 1-300Hz
 with tables.open_file(taste_dat.hdf5_name,'r') as hf5:
     whole_session_lfp_node = hf5.list_nodes('/Whole_session_raw_LFP') 
     whole_lfp = whole_session_lfp_node[0][:]
@@ -156,6 +157,14 @@ padding = 1 #second before taste delivery won't be extracted
 # (trials x channels x time)
 iti_array = np.asarray([whole_lfp[:,(x-(time_before_delivery*Fs)):(x-(padding*Fs))]\
         for x in delivery_times.delivery_time])
+
+
+# ____                  _                                           
+#/ ___| _ __   ___  ___| |_ _ __ ___   __ _ _ __ __ _ _ __ ___  ___ 
+#\___ \| '_ \ / _ \/ __| __| '__/ _ \ / _` | '__/ _` | '_ ` _ \/ __|
+# ___) | |_) |  __/ (__| |_| | | (_) | (_| | | | (_| | | | | | \__ \
+#|____/| .__/ \___|\___|\__|_|  \___/ \__, |_|  \__,_|_| |_| |_|___/
+#      |_|                            |___/                         
 
 # Mean spectrograms across all channels
 
@@ -233,34 +242,163 @@ plt.show()
 # Extract bandpassed lfp
 # Bandpass filter whole_lfp
 
-#define bandpass filter parameters to parse out frequencies
-#def butter_bandpass(lowcut, highcut, fs, order=2):
-#    nyq = 0.5 * fs
-#    low = lowcut / nyq
-#    high = highcut / nyq
-#    b, a = butter(order, [low, high], btype='band')
-#    return b, a
-#
-#def butter_bandpass_filter(data, lowcut, highcut, fs, order=2):
-#    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-#    y = filtfilt(b, a, data)
-#    return y
-#
-#
-#Fs = 1000 
-#bandpassed_lfp = np.asarray([
-#                    butter_bandpass_filter(
-#                        data = whole_lfp, 
-#                        lowcut = band[0],
-#                        highcut = band[1],
-#                        fs = Fs) \
-#                                for band in tqdm(band_freqs)])
-#
-## Plot to make sure everything is good in LFPs
-#down_bandpassed_lfp = bandpassed_lfp[:,:,np.arange(0,bandpassed_lfp.shape[-1],down_ratio)]
-#fig,ax = plt.subplots(2,1,sharex='all')
-#ax[0].plot(down_lfp.T)
-#ax[1].plot(down_bandpassed_lfp[0].T);plt.show()
+define bandpass filter parameters to parse out frequencies
+def butter_bandpass(lowcut, highcut, fs, order=2):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=2):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = filtfilt(b, a, data)
+    return y
+
+band_freqs = [(1,4),
+                (4,7),
+                (7,12),
+                (12,25),
+                (25,50)]
+
+
+Fs = 1000 
+
+# Bandpass lfp
+whole_lfp_bandpassed = np.asarray([
+                    butter_bandpass_filter(
+                        data = whole_lfp, 
+                        lowcut = band[0],
+                        highcut = band[1],
+                        fs = Fs) \
+                                for band in tqdm(band_freqs)])
+
+iti_lfp_bandpassed = np.asarray([
+                    butter_bandpass_filter(
+                        data = iti_array, 
+                        lowcut = band[0],
+                        highcut = band[1],
+                        fs = Fs) \
+                                for band in tqdm(band_freqs)])
+
+affective_lfp_bandpassed = np.asarray([
+                    butter_bandpass_filter(
+                        data = affective_dat.all_lfp_array, 
+                        lowcut = band[0],
+                        highcut = band[1],
+                        fs = Fs) \
+                                for band in tqdm(band_freqs)])
+
+# Calculate Hilbert and amplitude
+whole_lfp_hilbert = hilbert(whole_lfp_bandpassed)
+iti_lfp_hilbert = hilbert(iti_lfp_bandpassed)
+affective_lfp_hilbert = hilbert(affective_lfp_bandpassed)
+
+whole_lfp_amplitude = np.abs(whole_lfp_hilbert)
+iti_lfp_amplitude = np.abs(iti_lfp_hilbert)
+affective_lfp_amplitude = np.abs(affective_lfp_hilbert)
+
+
+# Plot to make sure it's working
+tmax = 100000
+plt.plot(whole_lfp_bandpassed[0,0,:tmax])
+plt.plot(whole_lfp_ampltidue[0,0,:tmax])
+plt.show()
+
+# Downsample amplitude to make it more amenable to plotting
+down_ratio = 10
+whole_lfp_amplitude_down = whole_lfp_amplitude\
+        [:,:,np.arange(0,whole_lfp_amplitude.shape[-1],down_ratio)]
+iti_lfp_amplitude_down = iti_lfp_amplitude\
+        [:,:,:,np.arange(0,iti_lfp_amplitude.shape[-1],down_ratio)]
+mean_iti_lfp_amplitude_down = np.mean(iti_lfp_amplitude_down,axis=-1).swapaxes(1,2)
+affective_lfp_amplitude_down = affective_lfp_amplitude\
+        [:,:,np.arange(0,affective_lfp_amplitude.shape[-1],down_ratio)]
+
+# Take average across channels
+# Entire taste session lfp
+this_dat = whole_lfp_amplitude_down 
+mean_this_dat = np.mean(this_dat,axis=1)
+std_this_dat = np.std(this_dat,axis=1)
+fig,ax = plt.subplots(len(this_dat),1,sharex=True)
+for ax_num,this_ax in enumerate(ax):
+    this_ax.fill_between(x=range(this_dat[ax_num].shape[-1]),
+            y1 = mean_this_dat[ax_num] - std_this_dat[ax_num],
+            y2= mean_this_dat[ax_num] + std_this_dat[ax_num],
+            alpha = 0.5,color='orange')
+    this_ax.plot(range(len(mean_this_dat[ax_num])),mean_this_dat[ax_num])
+plt.show()
+
+# Taste session ITIs 
+# Heatmaps
+mean_channel_iti_lfp_amp_down = np.mean(iti_lfp_amplitude_down,axis=2)
+zscore_channel_iti_lfp_amp_down = np.array([zscore(x,axis=None) \
+        for x in mean_channel_iti_lfp_amp_down])
+affective_dat.firing_overview(zscore_channel_iti_lfp_amp_down);plt.show()
+
+# Error bars
+this_dat = mean_iti_lfp_amplitude_down 
+mean_this_dat = np.mean(this_dat,axis=1)
+std_this_dat = np.std(this_dat,axis=1)
+fig,ax = plt.subplots(len(this_dat),1,sharex=True)
+for ax_num,this_ax in enumerate(ax):
+    this_ax.fill_between(x=range(this_dat[ax_num].shape[-1]),
+            y1 = mean_this_dat[ax_num] - std_this_dat[ax_num],
+            y2= mean_this_dat[ax_num] + std_this_dat[ax_num],
+            alpha = 0.5,color='orange')
+    this_ax.plot(range(len(mean_this_dat[ax_num])),mean_this_dat[ax_num])
+plt.show()
+
+# Affective session 
+this_dat = affective_lfp_amplitude_down 
+mean_this_dat = np.mean(this_dat,axis=1)
+std_this_dat = np.std(this_dat,axis=1)
+fig,ax = plt.subplots(len(this_dat),1,sharex=True)
+for ax_num,this_ax in enumerate(ax):
+    this_ax.fill_between(x=range(this_dat[ax_num].shape[-1]),
+            y1 = mean_this_dat[ax_num] - std_this_dat[ax_num],
+            y2= mean_this_dat[ax_num] + std_this_dat[ax_num],
+            alpha = 0.5,color='orange')
+    this_ax.plot(range(len(mean_this_dat[ax_num])),mean_this_dat[ax_num])
+plt.show()
+
+
+#############
+# Question:
+# Is ITI LFP more similar to different parts of the affective period
+# Or is ITI LFP power more similar to first or second half of LiCl affective recording
+############
+
+# Unroll iti_lfp_ampltiude
+iti_lfp_amplitude_down_long = iti_lfp_amplitude_down.swapaxes(1,2).\
+        reshape((iti_lfp_amplitude_down.shape[0],
+                    iti_lfp_amplitude_down.shape[2],
+                    -1))
+
+# Side by side plots of ITI and Affective bands 
+fig,ax = plt.subplots(len(this_dat),2,sharey='row')
+this_dat = iti_lfp_amplitude_down_long 
+mean_this_dat = np.mean(this_dat,axis=1)
+std_this_dat = np.std(this_dat,axis=1)
+for ax_num,this_ax in enumerate(ax[:,1]):
+    this_ax.fill_between(x=range(this_dat[ax_num].shape[-1]),
+            y1 = mean_this_dat[ax_num] - std_this_dat[ax_num],
+            y2= mean_this_dat[ax_num] + std_this_dat[ax_num],
+            alpha = 0.5,color='orange')
+    this_ax.plot(range(len(mean_this_dat[ax_num])),mean_this_dat[ax_num])
+this_dat = affective_lfp_amplitude_down 
+mean_this_dat = np.mean(this_dat,axis=1)
+std_this_dat = np.std(this_dat,axis=1)
+for ax_num,this_ax in enumerate(ax[:,0]):
+    this_ax.fill_between(x=range(this_dat[ax_num].shape[-1]),
+            y1 = mean_this_dat[ax_num] - std_this_dat[ax_num],
+            y2= mean_this_dat[ax_num] + std_this_dat[ax_num],
+            alpha = 0.5,color='orange')
+    this_ax.plot(range(len(mean_this_dat[ax_num])),mean_this_dat[ax_num])
+ax[0,0].title.set_text('Affective')
+ax[0,1].title.set_text('Taste ITI')
+plt.show()
+
 
 
 #    _                _           _     
@@ -285,12 +423,6 @@ def make_array_identifiers(array):
                     array.shape).flatten())
     return nd_idx_objs
 
-
-band_freqs = [(1,4),
-                (4,7),
-                (7,12),
-                (12,25),
-                (25,50)]
 
 # Mean band spectrograms
 # Average spectrogram power within single bands
@@ -393,11 +525,12 @@ mean_trial_df['trial_bin'] = pd.cut(mean_trial_df.trial,
         bins = trial_bin_num, include_lowest = True, labels = range(trial_bin_num))
 mean_trial_df['trial_bin'] = mean_trial_df['trial_bin'].astype(np.dtype('int16')) 
 
+mean_trial_df = mean_trial_df.astype('float')
+
 # Plot dataframe to visualize
 g = sns.FacetGrid(data = \
             mean_trial_df, col = 'band', hue = 'taste', sharey=False)
-g.map(sns.pointplot, 'trial_bin','power',ci='sd')
-plt.legend()
+g.map(sns.pointplot, 'trial_bin','power',ci='sd').add_legend()
 plt.show()
 
 
