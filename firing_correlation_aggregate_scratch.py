4b5cc505b688729a53b10b2d423c37288c9b4b61
abuzarmahmood <abuzarmahmood@gmail.com>
2020-02-13 11:43:15 -0500

    Finalizing functions for firing rate correlations

--- firing_correlation_aggregate_scratch.py
+++ firing_correlation_aggregate_scratch.py
@@ -1,4 +1,3 @@
-
 ## Import required modules
 import os
 import matplotlib.pyplot as plt
@@ -18,8 +17,6 @@ import shutil
 os.chdir('/media/bigdata/firing_space_plot/ephys_data')
 from ephys_data import ephys_data
 from scipy.signal import fftconvolve
-from scipy.stats import zscore
-from scipy.signal import savgol_filter as sk_ft
 
 ########################################################
 # Define functions and relevant variables to extract data
@@ -56,6 +53,7 @@ with tables.open_file(data_hdf5_path,'r') as hf5:
     spike_array_list =  [hf5.get_node(path,'spike_array')[:] for path in node_path_list]
 
 print('Calculating firing correlations')
+#for this_node_num in tqdm(range(len(phase_node_list))):
 
 this_node_num = 0
 
@@ -74,37 +72,22 @@ relative_channel_num_split = \
                 np.arange(len(electrode_nums))[~middle_channels_bool]]
 del firing_array, spike_array
 
-# Reshape spike_arrays to be total_steps x window_size arrays
-def step_break_array(array, window_size, step_size, zscore_bool=True):
-    total_time = array.shape[-1] 
-    bin_inds = (0,window_size)
-    total_bins = int((total_time - window_size + 1) / step_size) + 1
-    bin_list = [(bin_inds[0]+step,bin_inds[1]+step) \
-            for step in np.arange(total_bins)*step_size ]
-    array_steps = np.empty(tuple((*array.shape[:-1],total_bins,window_size)))
-    for bin_num,bin_inds in enumerate(bin_list):
-        array_steps[...,bin_num, :] = \
-                array[...,bin_inds[0]:bin_inds[1]]
-    if zscore_bool:
-        array_steps  = zscore(array_steps,axis=-1) 
-    return array_steps
 
-def step_convolve(array1,array2,window_size,step_size,zscore_bool=True):
-    # Arrays will be convolved along the last axis
-    # Require that both arrays have the same size
-    if array1.shape != array2.shape:
-        raise Exception('Arrays need to have same shape')
-    step_break_arrays = step_break_array(np.array([array1,array2]),
-            window_size,step_size,zscore_bool=zscore_bool)
-    out = fftconvolve(step_break_arrays[0],step_break_arrays[1],mode = 'same',axes=-1)
-    return np.squeeze(out)
+#  ____                    _       _   _             
+# / ___|___  _ __ _ __ ___| | __ _| |_(_) ___  _ __  
+#| |   / _ \| '__| '__/ _ \ |/ _` | __| |/ _ \| '_ \ 
+#| |__| (_) | |  | | |  __/ | (_| | |_| | (_) | | | |
+# \____\___/|_|  |_|  \___|_|\__,_|\__|_|\___/|_| |_|
+#                                                    
 
-# _____         _      ____               
-#|_   _|__  ___| |_   / ___|__ _ ___  ___ 
-#  | |/ _ \/ __| __| | |   / _` / __|/ _ \
-#  | |  __/\__ \ |_  | |__| (_| \__ \  __/
-#  |_|\___||___/\__|  \____\__,_|___/\___|
-#                                         
+# 1) Correlation of spike trains
+#   a) Convolve spike trains
+#   b) Convolve shuffled spike trains
+
+## Have this stored somewhere
+Fs = 1000
+time_vec = np.arange(0,7)/Fs
+
 # Generate test cases for correlations
 # Pair of spike trains with varying correlations
 # 1/4 : No correlation
@@ -144,22 +127,101 @@ ax[2].plot(np.arange(len(epochs[0]))*dt, firing_rate_list[0])
 ax[2].plot(np.arange(len(epochs[0]))*dt, firing_rate_list[1])
 plt.show()
 
-# Correlation parameters
-window_size = 0.5 #sec
-step_size = 0.5  #sec
+        
+# Reshape spike_arrays to be total_steps x window_size arrays
+def step_break_array(array, window_size, step_size, zscore_bool=True):
+    from scipy.stats import zscore
+    total_time = array.shape[-1] 
+    bin_inds = (0,window_size)
+    total_bins = int((total_time - window_size + 1) / step_size) + 1
+    bin_list = [(bin_inds[0]+step,bin_inds[1]+step) \
+            for step in np.arange(total_bins)*step_size ]
+    array_steps = np.empty(tuple((*array.shape[:-1],total_bins,window_size)))
+    for bin_num,bin_inds in enumerate(bin_list):
+        array_steps[...,bin_num, :] = \
+                array[...,bin_inds[0]:bin_inds[1]]
+    if zscore_bool:
+        array_steps  = zscore(array_steps,axis=-1) 
+    return array_steps
 
-# Spike train correlation
+#test = step_break_vector(spike_train_array, int(window_size/dt), int(step_size/dt))
+
+#plt.imshow(test,interpolation='gaussian',aspect='auto',cmap='jet');plt.show()
+
+
+# Moving window spike correlations
+window_size = 5 #sec
+step_size = 5  #sec
+# Convert array and convolve
+#step_break_arrays = [step_break_vector(x,int(window_size/dt), int(step_size/dt))\
+#        for x in spike_train_list]
+broken_array = step_break_array(np.array(spike_train_list),
+        int(window_size/dt), int(step_size/dt),zscore_bool = False)
+
+plt.subplot(121)
+plt.imshow(broken_array[0],interpolation='gaussian',aspect='auto',cmap='jet')
+plt.subplot(122)
+plt.imshow(broken_array[1],interpolation='gaussian',aspect='auto',cmap='jet')
+plt.show()
+
+def step_convolve(array1,array2,window_size,step_size):
+    # Arrays will be convolved along the last axis
+    # Require that both arrays have the same size
+    from scipy.signal import fftconvolve
+    if array1.shape != array2.shape:
+        raise Exception('Arrays need to have same shape')
+    broken_array = step_break_array(np.array([array1,array2]),window_size,step_size)
+    out = fftconvolve(broken_array[0],broken_array[1],mode = 'same',axes=-1)
+    return np.squeeze(out)
+
 out = step_convolve(spike_train_list[0],spike_train_list[1],
-        int(window_size/dt), int(step_size/dt),zscore_bool=False)
+        int(window_size/dt), int(step_size/dt))
 plt.pcolormesh(np.linspace(-window_size/2,window_size/2,out.shape[-1]),
-        np.linspace(0,len(epochs[0])*dt,out.shape[0]),out,cmap='jet')
+        np.linspace(0,len(epochs[0])*dt,out.shape[0]),out)
 plt.xlabel('Time lag (sec)')
 plt.ylabel('Window center (sec)')
 plt.show()
 
-# Firing rate correlations
+
+# Perform convolution
+out = fftconvolve(step_break_arrays[0],step_break_arrays[1],mode = 'same',axes=-1)
+plt.pcolormesh(np.linspace(-window_size/2,window_size/2,out.shape[-1]),
+        np.linspace(0,len(epochs[0])*dt,out.shape[0]),out)
+plt.xlabel('Time lag (sec)')
+plt.ylabel('Window center (sec)')
+plt.show()
+
+plt.imshow(out,interpolation='gaussian',aspect='auto',cmap='jet', origin='lower');plt.show()
+
+# 2) Correlation of firing rates
+## Zscore each window of firing for both neurons separately so
+## spike count differences can be ruled out
+window_size = 0.5 #sec
+step_size = 0.5  #sec
+# Convert array and convolve
+firing_break_arrays = [step_break_vector(x,int(window_size/dt), int(step_size/dt))\
+        for x in firing_rate_list]
+firing_break_arrays = step_break_vector(np.array(firing_rate_list),int(window_size/dt), int(step_size/dt))
+
+plt.subplot(121)
+plt.imshow(firing_break_arrays[0],interpolation='gaussian',aspect='auto',cmap='jet')
+plt.subplot(122)
+plt.imshow(firing_break_arrays[1],interpolation='gaussian',aspect='auto',cmap='jet')
+plt.show()
+
+zscore_firing_break_arrays = [zscore(x,axis=-1) for x in firing_break_arrays]
+plt.subplot(121)
+plt.imshow(zscore_firing_break_arrays[0],interpolation='gaussian',aspect='auto',cmap='jet')
+plt.subplot(122)
+plt.imshow(zscore_firing_break_arrays[1],interpolation='gaussian',aspect='auto',cmap='jet')
+plt.show()
+
+from scipy.signal import savgol_filter as sk_ft
+# Perform convolution
+out = fftconvolve(zscore_firing_break_arrays[0],zscore_firing_break_arrays[1],mode = 'same',axes=-1)
+
 out = step_convolve(firing_rate_list[0],firing_rate_list[1],
-        int(window_size/dt), int(step_size/dt),zscore_bool=True)
+        int(window_size/dt), int(step_size/dt))
 # Test histogram
 bin_num = 10
 percentiles = np.arange(0,100,100//bin_num)
