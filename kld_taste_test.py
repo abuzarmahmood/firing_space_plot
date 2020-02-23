8773f3d05bbe02e3003d67a638ff93f2d3774dec
abuzarmahmood <abuzarmahmood@gmail.com>
2020-02-22 19:57:55 -0500

    Added shuffle code

--- kld_taste_test.py
+++ kld_taste_test.py
@@ -9,20 +9,18 @@ import os
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Qt5Agg')
-import lspopt
 import tables
 import easygui
 import scipy
-from scipy.signal import spectrogram
-from lspopt import spectrogram_lspopt
 import numpy as np
-from scipy.signal import hilbert, butter, filtfilt,freqs 
 from tqdm import tqdm, trange
 from sklearn.utils import resample
 from itertools import product
 import scipy.signal as signal
 import itertools
 from scipy.special import gamma
+from joblib import Parallel,delayed
+import multiprocessing as mp
 
 def kl_divergence(vec_a,vec_b):
     """
@@ -84,7 +82,7 @@ dat.firing_overview(dat.all_normalized_firing);plt.show()
 # Generate firing rate distribution for each taste
 
 # Calculate firing rates using BAKS
-nrn_num = 18
+nrn_num = 2
 this_nrn_spikes = np.array(dat.spikes)[:,:,nrn_num]
 # If the neuron doesn't spike in a trial, throw a single spike somewhere
 for inds in zip(*np.where(np.sum(this_nrn_spikes,axis=-1)==0)):
@@ -169,49 +167,13 @@ taste_discrim_time = np.sum(kld_array,axis=0)
 plt.plot(taste_discrim_time);plt.show()
 taste_discrim = np.sum(kld_array,axis=None)
 
-#def calc_taste_discrim(this_nrn):
-#    """
-#    this_nrn : (taste,trials,time)
-#    """
-#    # Chop firing rates in quartile and time bins for each taste
-#    symbols = 5
-#    quartiles = np.linspace(0,100,symbols+1)
-#    quart_vals = np.percentile(this_nrn.flatten(),quartiles)
-#    time_bin_count = 40
-#    time_bins = list(map(int,np.floor(np.linspace(0,this_nrn.shape[2],time_bin_count+1))))
-#
-#    cpd = np.empty((this_nrn.shape[0],symbols,time_bin_count))
-#    for taste in range(this_nrn.shape[0]):
-#        for time_bin_num in range(1,len(time_bins)):
-#            cpd[taste,:,time_bin_num-1] = \
-#                    np.histogram(this_nrn[taste,:,time_bins[time_bin_num-1]:time_bins[time_bin_num]],bins = quart_vals)[0]
-#
-#    # Add some noise to CPD to avoid zero errors
-#    cpd += np.random.random(cpd.shape)*1e-9
-#
-#    # Normalize CPD within each bin
-#    norm_cpd = cpd / np.sum(cpd,axis=(1))[:,np.newaxis]
-#
-#    # List all possible combinations of pairwise taste comparisons
-#    taste_comparisons = list(itertools.combinations(range(this_nrn.shape[0]),2))
-#
-#    kld_array = np.empty((len(taste_comparisons),time_bin_count))
-#    for pair_num, pair in enumerate(taste_comparisons):
-#        for time_bin in range(cpd.shape[-1]):
-#            kld_array[pair_num,time_bin] = \
-#                kl_divergence(cpd[pair[0],:,time_bin], cpd[pair[1],:,time_bin])
-#
-#    return kld_array
-
-def calc_taste_discrim2(this_nrn, taste_labels):
+def calc_taste_discrim(this_nrn, taste_labels, symbols = 5, time_bin_count = 40):
     """
-    this_nrn : (taste,trials,time)
+    this_nrn : (trials,time)
     """
     # Chop firing rates in quartile and time bins for each taste
-    symbols = 5
     quartiles = np.linspace(0,100,symbols+1)
     quart_vals = np.percentile(this_nrn.flatten(),quartiles)
-    time_bin_count = 40
     time_bins = list(map(int,np.floor(np.linspace(0,this_nrn.shape[-1],time_bin_count+1))))
 
     cpd = np.empty((len(np.unique(taste_labels)),symbols,time_bin_count))
@@ -252,17 +214,51 @@ for nrn_num,this_nrn_spikes in enumerate(np.array(all_spikes)):
             for taste in this_nrn_spikes])
 all_firing_long = np.reshape(all_firing,(all_firing.shape[0],-1,all_firing.shape[-1]))
 
-#test1 = calc_taste_discrim(all_firing[0])
-#test2 = calc_taste_discrim2(all_firing_long[0],taste_labels)
-#
-#plt.subplot(211)
-#dat.imshow(test1)
-#plt.subplot(212)
-#dat.imshow(test2)
-#plt.show()
-
 taste_discrim_vec = np.array([calc_taste_discrim(nrn,taste_labels) \
         for nrn in all_firing_long[...,26:]])
 
 # Plot firing of neurons sorted by taste discriminability
 dat.firing_overview(dat.all_normalized_firing[np.argsort(np.max(taste_discrim_vec,axis=(1,2)))]);plt.show()
+
+# Wrapper function to generate both pairwise KLDs and shuffles
+def shuffle_taste_discrim(this_nrn, taste_labels, shuffle_count = 100,
+        symbols = 5, time_bin_count = 40): 
+    """
+    this_nrn = (trials x time) # All trials concatenated
+    """
+    random_gen = [np.random.permutation(this_nrn) for repeat in range(shuffle_count)]
+    #random_klds = Parallel(n_jobs = mp.cpu_count()-2)\
+    #        (delayed(calc_taste_discrim)(this_shuffle,taste_labels) for this_shuffle in random_gen)
+    random_klds = np.array([calc_taste_discrim(this_shuffle,taste_labels,
+                                        symbols = symbols, time_bin_count = time_bin_count) \
+            for this_shuffle in random_gen])
+    return random_klds
+
+# Plot all_comparison vs shuffle
+nrn = 19
+this_taste_discrim = calc_taste_discrim(all_firing_long[nrn],taste_labels, time_bin_count = 50)
+this_taste_shuffle = shuffle_taste_discrim(all_firing_long[nrn],
+        taste_labels,time_bin_count = 50).swapaxes(0,1)
+
+dat.imshow(all_firing_long[nrn]);plt.show()
+
+from scipy.stats import gaussian_kde
+
+fig, ax = plt.subplots(this_taste_discrim.shape[0],1, sharey=True, sharex=True)
+#ax[0].set_ylim([np.min(this_taste_discrim,axis=None),np.max(this_taste_discrim,axis=None)])
+for taste_num, (taste, shuffle) in enumerate(zip(this_taste_discrim,this_taste_shuffle)):
+    ax[taste_num].plot(taste, '-x',c='orange')
+    mean_shuffle = np.mean(shuffle,axis=0)
+    std_shuffle = np.std(shuffle,axis=0)
+    shuffle_x = np.broadcast_to(np.arange(len(mean_shuffle))[np.newaxis,:],
+                    shuffle.shape).flatten()
+    shuffle_flat = shuffle.flatten()
+    #xy = np.vstack([shuffle_x,shuffle_flat])
+    #z = gaussian_kde(xy)(xy)
+    #ax[taste_num].scatter(shuffle_x + np.random.random(shuffle_x.shape), 
+    #        shuffle_flat, c=z, edgecolor='')
+    #ax[taste_num].hist2d(shuffle_x,shuffle_flat,(len(mean_shuffle),50))
+    ax[taste_num].fill_between(np.arange(len(taste)),
+            mean_shuffle - 2*std_shuffle, mean_shuffle + 2*std_shuffle,
+            alpha = 0.5, color = 'blue')
+plt.show()
