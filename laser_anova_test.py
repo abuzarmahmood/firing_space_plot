28fb4795a031d22eb7804e037e3d575c59d74f35
abuzarmahmood <abuzarmahmood@gmail.com>
2020-05-08 18:08:43 -0400

    Midway change backup

--- laser_anova_test.py
+++ laser_anova_test.py
@@ -1,4 +1,5 @@
 ## Import required modules
+import glob
 import os
 import matplotlib.pyplot as plt
 import matplotlib
@@ -9,50 +10,163 @@ import scipy
 import numpy as np
 from tqdm import tqdm, trange
 from joblib import Parallel,delayed
+import json
 import pingouin
 import pandas as pd
 import seaborn as sns
 from matplotlib_venn import venn2, venn2_circles, venn2_unweighted
 from matplotlib_venn import venn3, venn3_circles
 from sklearn.decomposition import PCA as pca
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as lda
 from scipy.stats import zscore
-
 os.chdir('/media/bigdata/firing_space_plot/ephys_data')
 from ephys_data import ephys_data
+import visualize
 
-# Extract data
-dat = \
-    ephys_data('/media/bigdata/Abuzar_Data/AM21/AM21_4Tastes_200305_085032')
-    #ephys_data('/media/bigdata/Abuzar_Data/AM21/AM21_4Tastes_200306_085755/')
-dat.firing_rate_params = dict(zip(('step_size','window_size','dt'),
-                                    (25,250,1)))
+# ___       _ _   _       _ _          _   _             
+#|_ _|_ __ (_) |_(_) __ _| (_)______ _| |_(_) ___  _ __  
+# | || '_ \| | __| |/ _` | | |_  / _` | __| |/ _ \| '_ \ 
+# | || | | | | |_| | (_| | | |/ / (_| | |_| | (_) | | | |
+#|___|_| |_|_|\__|_|\__,_|_|_/___\__,_|\__|_|\___/|_| |_|
+#                                                        
 
-dat.extract_and_process()
-dat.separate_laser_data()
+# Create file to store data + analyses in
+file_list_path = '/media/bigdata/firing_space_plot/don_grant_figs/laser_files.txt'
+plot_dir = os.path.join(os.path.dirname(file_list_path),'plots')
+if not os.path.exists(plot_dir):
+    os.makedirs(plot_dir)
 
-#dat.firing_overview(dat.all_on_lfp.swapaxes(0,1),zscore_bool=True)
-#dat.firing_overview(dat.all_off_lfp.swapaxes(0,1),zscore_bool=True)
-#plt.show()
+file_list = open(file_list_path,'r').readlines()
+file_list = [x.rstrip() for x in file_list]
+basename_list = [os.path.basename(x) for x in file_list]
+name_date_str_list = ['_'.join([x.split('_')[0],x.split('_')[2]]) \
+        for x in basename_list]
+dirname_list = [os.path.dirname(x) for x in file_list]
+json_path_list = [glob.glob(dirname+'/*json')[-1] for dirname in dirname_list]
 
-on_hilbert = hilbert(dat.all_on_lfp)
-off_hilbert = hilbert(dat.all_off_lfp)
+# Find all electrodes corresponding to BLA and GC using the jsons
+json_list = [json.load(open(file,'r')) for file in json_path_list]
+bla_electrodes = [x['regions']['bla'] for x in json_list]
+gc_electrodes = [x['regions']['gc'] for x in json_list]
 
-on_spikes = dat.on_spikes
-off_spikes = dat.off_spikes
+all_on_firing = []
+all_off_firing = []
+all_unit_descriptors = []
+for file_num, file_name in tqdm(enumerate(file_list)):
+    data = ephys_data(data_dir = dirname_list[file_num]) 
+    data.firing_rate_params = dict(zip(\
+        ('type', 'step_size','window_size','dt', 'baks_resolution', 'baks_dt'),
+        ('conv',25,250,1,25e-3,1e-3)))
+    data.get_unit_descriptors()
+    data.get_spikes()
+    data.get_firing_rates()
+    data.separate_laser_firing()
+    all_on_firing.append(data.on_firing)
+    all_off_firing.append(data.off_firing)
+    all_unit_descriptors.append(data.unit_descriptors)
+
+# Remove cases where size of firing array doesn't match with
+# unit descriptors
+# WTF WOULD THAT HAPPEN THOUGH???
+
+all_unit_electrodes = [x["electrode_number"] for x in all_unit_descriptors]
+matchings_inds = [num for num, (firing, electrodes) in \
+        enumerate(zip(all_on_firing, all_unit_electrodes)) \
+        if firing.shape[2] == len(electrodes)]
+
+fin_filenames = [name_date_str_list[x] for x in matchings_inds]
+fin_on_firing = [all_on_firing[x] for x in matchings_inds]
+fin_off_firing = [all_off_firing[x] for x in matchings_inds]
+fin_unit_electrodes = [all_unit_electrodes[x] for x in matchings_inds]
+fin_gc_electrodes = [gc_electrodes[x] for x in matchings_inds]
+fin_bla_electrodes = [bla_electrodes[x] for x in matchings_inds]
+
+gc_unit_inds = [np.where(np.isin(unit_electrodes, this_gc_electrodes))[-1]\
+        for unit_electrodes, this_gc_electrodes \
+        in zip(fin_unit_electrodes, fin_gc_electrodes)]
+bla_unit_inds = [np.where(np.isin(unit_electrodes, this_bla_electrodes))[-1]\
+        for unit_electrodes, this_bla_electrodes \
+        in zip(fin_unit_electrodes, fin_bla_electrodes)]
+gc_nrn_count = [len(x) for x in gc_unit_inds]
+bla_nrn_count = [len(x) for x in bla_unit_inds]
+
+gc_on_list = [firing[:,:,inds] for firing,inds in zip(fin_on_firing, gc_unit_inds)]
+gc_off_list = [firing[:,:,inds] for firing,inds in zip(fin_off_firing, gc_unit_inds)]
+bla_on_list = [firing[:,:,inds] for firing,inds in zip(fin_on_firing, bla_unit_inds)]
+bla_off_list = [firing[:,:,inds] for firing,inds in zip(fin_off_firing, bla_unit_inds)]
+
+gc_on_array = np.moveaxis(np.concatenate(gc_on_list, axis = 2),2,0)
+gc_off_array = np.moveaxis(np.concatenate(gc_off_list, axis = 2),2,0)
+bla_on_array = np.moveaxis(np.concatenate(bla_on_list, axis = 2),2,0)
+bla_off_array = np.moveaxis(np.concatenate(bla_off_list, axis = 2),2,0)
+
+time_lims = (2000,4500)
+step_size = data.firing_rate_params['step_size']
+bin_lims = [x//step_size for x in time_lims]
+gc_firing = np.stack([gc_on_array, gc_off_array])
+bla_firing = np.stack([bla_on_array, bla_off_array])
+gc_firing = gc_firing[...,bin_lims[0]:bin_lims[1]]
+bla_firing = bla_firing[...,bin_lims[0]:bin_lims[1]]
+
+gc_dim_inds = np.stack(list(np.ndindex(gc_firing.shape))).T
+bla_dim_inds = np.stack(list(np.ndindex(bla_firing.shape))).T
+
+gc_firing_frame = pd.DataFrame(\
+        data = {'laser' : gc_dim_inds[0],
+                'neuron' : gc_dim_inds[1],
+                'taste' : gc_dim_inds[2],
+                'trial' : gc_dim_inds[3],
+                'time' : gc_dim_inds[4],
+                'firing' : gc_firing.flatten()}) \
+
+bla_firing_frame = pd.DataFrame(\
+        data = {'laser' : bla_dim_inds[0],
+                'neuron' : bla_dim_inds[1],
+                'taste' : bla_dim_inds[2],
+                'trial' : bla_dim_inds[3],
+                'time' : bla_dim_inds[4],
+                'firing' : bla_firing.flatten()}) \
 
 # Bin firing into larger bins for anova
-time_lims = (2000,4500)
 bin_width = 250
-fs = 1000
+bin_width_in_inds = bin_width // step_size
+fin_bin_count = gc_firing.shape[-1]//bin_width_in_inds
 
-binned_on_firing = np.sum(np.reshape(on_spikes[...,time_lims[0]:time_lims[1]],
-                    (*on_spikes.shape[:3],bin_width,-1)),axis=-2)/ (bin_width/fs)
-binned_off_firing = np.sum(np.reshape(off_spikes[...,time_lims[0]:time_lims[1]],
-                    (*off_spikes.shape[:3],bin_width,-1)),axis=-2)/ (bin_width/fs)
+gc_bin_firing_frame = gc_firing_frame.copy()
+gc_bin_firing_frame['bin'] = pd.cut(gc_firing_frame.time,
+        bins = fin_bin_count, include_lowest = True, 
+        labels = np.arange(fin_bin_count))
 
-dim_inds = np.stack(list(np.ndindex(binned_on_firing.shape))).T
+bla_bin_firing_frame = bla_firing_frame.copy()
+bla_bin_firing_frame['bin'] = pd.cut(bla_firing_frame.time,
+        bins = fin_bin_count, include_lowest = True, 
+        labels = np.arange(fin_bin_count))
+
+# Fix things up after binning
+gc_bin_firing_frame.drop('time',inplace=True,axis=1)
+gc_bin_firing_frame = \
+gc_bin_firing_frame.groupby(['laser','neuron','taste','trial','bin'])\
+                .mean().reset_index()
+
+bla_bin_firing_frame.drop('time',inplace=True,axis=1)
+bla_bin_firing_frame = \
+bla_bin_firing_frame.groupby(['laser','neuron','taste','trial','bin'])\
+                .mean().reset_index()
+
+#binned_on_firing = np.sum(np.reshape(on_spikes[...,time_lims[0]:time_lims[1]],
+#                    (*on_spikes.shape[:3],bin_width,-1)),axis=-2)/ (bin_width/fs)
+#binned_off_firing = np.sum(np.reshape(off_spikes[...,time_lims[0]:time_lims[1]],
+#                    (*off_spikes.shape[:3],bin_width,-1)),axis=-2)/ (bin_width/fs)
+
 all_electrode_numbers = [x['electrode_number'] for x in dat.unit_descriptors]
+
+##################################################
+# ____  _             _        _   _            
+#/ ___|(_)_ __   __ _| | ___  | \ | |_ __ _ __  
+#\___ \| | '_ \ / _` | |/ _ \ |  \| | '__| '_ \ 
+# ___) | | | | | (_| | |  __/ | |\  | |  | | | |
+#|____/|_|_| |_|\__, |_|\___| |_| \_|_|  |_| |_|
+#               |___/                           
+##################################################
 
 firing_frame_list = [pd.DataFrame(\
         data = {'taste' : dim_inds[0],
@@ -119,8 +233,10 @@ electrode_neuron = [(all_electrode_numbers[num],num) for num in range(len(sig_wa
 #        for x in dat.unit_descriptors[relevant_laser]])
 port_a = np.arange(32)
 port_b = np.arange(32,64)
-port_a_present = [x for x,wav in zip(electrode_neuron,sig_waveform) if (x[0] in port_a) and wav]
-port_b_present = [x for x,wav in zip(electrode_neuron,sig_waveform) if (x[0] in port_b) and wav]
+port_a_present = [x for x,wav in zip(electrode_neuron,sig_waveform) \
+                                                if (x[0] in port_a) and wav]
+port_b_present = [x for x,wav in zip(electrode_neuron,sig_waveform) \
+                                                if (x[0] in port_b) and wav]
 laser_present = [x for x,laser in zip(electrode_neuron,relevant_laser) if laser]
 v3 = venn3([set(port_a_present),
             set(port_b_present),
