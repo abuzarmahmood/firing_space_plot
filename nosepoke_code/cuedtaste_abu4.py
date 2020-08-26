df3bf8160e333273027dfd1f748352f1dc2a8ab1
abuzarmahmood <abuzarmahmood@gmail.com>
2020-08-26 17:10:00 -0400

    Working version with laser and taste

--- nosepoke_code/cuedtaste_abu4.py
+++ nosepoke_code/cuedtaste_abu4.py
@@ -10,6 +10,35 @@ import datetime
 import numpy as np
 GPIO.setmode(GPIO.BOARD)
 
+taste_out = 31
+intan_taste = 24
+laser_out = 12
+intan_laser = 8
+taste_ports = [taste_out, intan_taste]
+laser_ports = [laser_out, intan_laser]
+opentime = 0.25
+laser_duration = 2.5
+# Set the outports to outputs
+for i in [taste_out, intan_taste, laser_out, intan_laser]:
+        GPIO.setup(i, GPIO.OUT)
+        GPIO.output(i, 0)
+
+def trigger_action(taste_ports, laser_ports, opentime, laser_duration):
+    """
+    lists of ports for taste and laser
+    """
+    time_diff = np.float(laser_duration) - np.float(opentime)
+    for port in taste_ports:  
+        GPIO.output(port,1)
+    for port in laser_ports:  
+        GPIO.output(port,1)
+    time.sleep(opentime)
+    for port in taste_ports:
+        GPIO.output(port,0)
+    time.sleep(time_diff)
+    for port in laser_ports:
+        GPIO.output(port,0)
+
 class nosepoke_thread(Thread):
     """
     Class to control cue and activate outputs 
@@ -20,6 +49,7 @@ class nosepoke_thread(Thread):
         nosepoke_gpio :: Which port to read from
         freq :: Frequency of readings
         """
+        # Inititate thread details
         Thread.__init__(self)
         self.threadID = threadID
         self.name = name
@@ -27,9 +57,12 @@ class nosepoke_thread(Thread):
         # Initialize board details
         GPIO.setup(nosepoke_gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+        # Save pin numbers
         self.nosepoke_gpio = nosepoke_gpio
         self.taste_output = taste_output
         self.laser_output = laser_output
+
+        # Set run parameters
         self.freq = np.float(freq)
         self.cue_on = cue_on
         self.cue_off = cue_off
@@ -48,29 +81,29 @@ class nosepoke_thread(Thread):
     def update(self):
         # Keep looping indefinitely till thread is stopped
         while True:
-            time.sleep(1/self.freq)
-            print('iter')
-            if datetime.datetime.now() > self.wait_till:
-                self.cue_on()
-                print('ON')
-            if self.stopped:
-                return
-            temp_read = GPIO.input(self.nosepoke_gpio)
-            if not temp_read: # Assuming 1 indicates poke
-                self.action_check()
+            if not self.stopped:
+                time.sleep(1/self.freq)
+                #print('iter')
+                if datetime.datetime.now() > self.wait_till:
+                    self.cue_on()
+                    #print('ON')
+                temp_read = GPIO.input(self.nosepoke_gpio)
+                if not temp_read: # Assuming 1 indicates poke
+                    self.action_check()
 
     def action_check(self):
         """
         Checks whether action should be allowed to pass
         """
-        print('check initiated')
+        #print('check initiated')
         current_time = datetime.datetime.now()
         if current_time > self.wait_till:
             self.cue_off()
-            print('OFF')
+            #print('OFF')
             self.latest_trigger_time = current_time
             self.wait_till = current_time + self.iti_delta
-            print("ACTION COMPLETED")
+            trigger_action(taste_ports, laser_ports, opentime, laser_duration)
+            #print("ACTION COMPLETED")
         return
 
     def cue_status(self):
@@ -116,6 +149,8 @@ class cue_thread(Thread):
 ########################################
 # Initiate run
 ########################################
+trigger_action(taste_ports, laser_ports, opentime, laser_duration)
+
 
 light = 36
 poke_sample_freq = 100
@@ -128,4 +163,5 @@ poke_thread = nosepoke_thread(2, 'poke_thread', beam,poke_sample_freq,1,1,10,
             cue_thread1.release_stop, cue_thread1.set_stop)
 poke_thread.start()
 
-
+poke_thread.set_stop()
+cue_thread1.set_stop()
