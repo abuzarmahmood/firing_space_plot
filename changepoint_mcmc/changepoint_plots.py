f23edf81acd4526381dfb893f2e334693a5874da
abuzarmahmood <abuzarmahmood@gmail.com>
2021-02-13 21:50:08 -0500

    Refactor code to generate raster-changepoint overlay plots more streamlinedly and use new naming convention

--- changepoint_mcmc/changepoint_plots.py
+++ changepoint_mcmc/changepoint_plots.py
@@ -40,21 +40,17 @@ import poisson_all_tastes_changepoint_model as changepoint
 #|_____\___/ \__,_|\__,_| |____/ \__,_|\__\__,_|
 ############################################################
 
-#params_file_path = \
-#        '/media/bigdata/firing_space_plot/changepoint_mcmc/fit_params.json'
+parser = argparse.ArgumentParser(description = 'Script to fit changepoint model')
+parser.add_argument('model_path',  help = 'Path to model pkl file')
+args = parser.parse_args()
+model_path = args.model_path 
 
-#parser = argparse.ArgumentParser(description = 'Script to fit changepoint model')
-#parser.add_argument('dir_name',  help = 'Directory containing data files')
-#parser.add_argument('states', type = int, help = 'Number of States to fit')
-#args = parser.parse_args()
-#data_dir = args.dir_name 
+#model_path = '/media/bigdata/Abuzar_Data/AM35/AM35_4Tastes_201230_115322/'\
+#        'saved_models/vi_4_states/dump_vi_4states_40000fit_1500_4000time_50bin.pkl'
 
-#data_dir = '/media/bigdata/Abuzar_Data/AM35/AM35_4Tastes_201230_115322/'
-#states = 4
-
-model_path = '/media/bigdata/Abuzar_Data/AM35/AM35_4Tastes_201230_115322/'\
-        'saved_models/vi_4_states/dump_vi_4states_40000fit_1500_4000time_50bin.pkl'
-
+##########
+# PARAMS 
+##########
 # Extract model params from basename
 model_name = os.path.basename(model_path)
 states = int(re.findall("\d+states",model_name)[0][:-6])
@@ -74,24 +70,9 @@ dat.default_stft_params['max_freq'] = 50
 dat.get_stft(recalculate=False, dat_type = ['amplitude'])
 dat.get_region_units()
 
-##########
-# PARAMS 
-##########
-#states = int(args.states)
-
-#with open(params_file_path, 'r') as file:
-#    params_dict = json.load(file)
-#
-#for key,val in params_dict.items():
-#    globals()[key] = val
-
 ########################################
 # Create dirs and names
 ########################################
-#model_save_dir = changepoint.get_model_save_dir(data_dir, states)
-#model_name = changepoint.get_model_name(states,fit,time_lims,bin_width,'shuffle')
-#model_dump_path = changepoint.get_model_dump_path(model_name,model_save_dir)
-
 plot_super_dir = os.path.join(data_dir,'changepoint_plots')
 plot_dir = os.path.join(plot_super_dir,model_name)
 
@@ -100,9 +81,9 @@ if not os.path.exists(plot_super_dir):
 if not os.path.exists(plot_dir):
         os.makedirs(plot_dir)
 
-if os.path.exists(model_dump_path):
+if os.path.exists(model_path):
     print('Trace loaded from cache')
-    with open(model_dump_path, 'rb') as buff:
+    with open(model_path, 'rb') as buff:
         data = pickle.load(buff)
     model = data['model']
     approx = data['approx']
@@ -169,6 +150,48 @@ plt.close(fig)
 ##################################################
 # Changepoint Plot
 ##################################################
+def create_changepoint_plots(spike_array, stft_array, tau_samples, trial_inds_list, 
+                    suptitle, taste_label, region_units_list, 
+                    stft_ticks, stft_tick_inds, 
+                    binned_tick_inds, binned_tick_vals,
+                    plot_type = 'raster'):
+
+    mean_tau = np.mean(tau_samples,axis=0)
+    mean_tau_stft = (mean_tau/spike_array.shape[-1])*stft_cut.shape[-1]
+
+    for fig_num in tqdm(np.arange(len(trial_inds_list))):
+        trial_inds = trial_inds_list[fig_num]
+        trial_count = len(trial_inds)
+        
+        fig, ax = plt.subplots(trial_count,3,#sharex='col', 
+                            figsize = (20,trial_count*3))
+        for num,trial in enumerate(trial_inds):
+            if plot_type == 'heatmap':
+                ax[num,0].imshow(spike_array[trial], **imshow_kwargs)
+            elif plot_type == 'raster':
+                ax[num,0].scatter(*np.where(spike_array[trial])[::-1], marker = "|")
+            ax[num,0].set_ylabel(taste_label[trial])
+            ax[num,1].imshow(stft_array[trial], **imshow_kwargs)
+            ax[num,0].hlines(len(region_units_list[0]) -0.5 ,**hline_kwargs)
+            ax[num,0].vlines(mean_tau[trial],-0.5,spike_array.shape[1]-0.5,
+                                **vline_kwargs)
+            ax[num,1].vlines(mean_tau_stft[trial],-0.5,stft_cut.shape[1]-0.5,
+                                **vline_kwargs)
+
+            ax[num,1].set_xticks(stft_tick_inds)
+            ax[num,1].set_xticklabels(stft_ticks[stft_tick_inds],rotation='vertical')
+
+            for state in range(tau_samples.shape[-1]):
+                ax[num,-1].hist(tau_samples[:,trial,state], 
+                    bins = 100, density = True)
+        ax[-1,0].set_xticks(binned_tick_inds)
+        ax[-1,0].set_xticklabels(binned_tick_vals, rotation = 'vertical')
+        ax[-1,-1].set_xticks(binned_tick_inds)
+        ax[-1,-1].set_xticklabels(binned_tick_vals, rotation = 'vertical')
+
+        fig.suptitle(suptitle)
+        fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+
 # Sort units be region
 # Mark change in region using hline
 unit_order = np.concatenate(dat.region_units)
@@ -181,6 +204,7 @@ stft_cut = stats.zscore(dat.amplitude_array[:,:],axis=-1)
 stft_cut = stft_cut[:,channel,...,time_lims[0]:time_lims[1]]
 stft_cut = np.reshape(stft_cut,(-1,*stft_cut.shape[2:]))
 stft_ticks = dat.time_vec[time_lims[0]:time_lims[1]]*1000
+stft_ticks = np.vectorize(np.int)(stft_ticks)
 stft_tick_inds = np.arange(0,len(stft_ticks),250)
 
 mean_tau_stft = (mean_tau/binned_dat.shape[-1])*stft_cut.shape[-1]
@@ -203,10 +227,10 @@ imshow_kwargs = {'interpolation':'nearest','aspect':'auto','origin':'lower'}
 
 region_unit_count = dict(zip(dat.region_names,[len(x) for x in dat.region_units]))
 
-trial_inds_list = [np.where(taste_label==this_taste)[0] \
+full_trial_inds_list = [np.where(taste_label==this_taste)[0] \
         for this_taste in np.sort(np.unique(taste_label))]
 
-create_plots(plot_spikes, stft_cut, tau_samples, trial_inds_list,
+create_changepoint_plots(plot_spikes, stft_cut, tau_samples, full_trial_inds_list,
             region_unit_count, taste_label, dat.region_units,
             stft_ticks, stft_tick_inds,
             binned_tick_inds, binned_tick_vals)
@@ -217,38 +241,6 @@ for fig_num in tqdm(plt.get_fignums()):
             this_plot_dir,'taste{}_changepoints'.format(fig_num)),dpi=300)
     plt.close(fig_num)
                 
-
-#for this_taste in np.sort(np.unique(taste_label)):
-#    trial_inds = np.where(taste_label==this_taste)[0]
-#
-#    fig, ax = plt.subplots(len(trial_inds),3,sharex='col', figsize = (15,50))
-#    for num,trial in enumerate(trial_inds):
-#        #ax[num,0].imshow(plot_spikes[trial], **imshow_kwargs)
-#        ax[num,0].scatter(*np.where(plot_spikes[trial])[::-1], marker = "|")
-#        ax[num,0].set_ylabel(taste_label[trial])
-#        ax[num,1].imshow(stft_cut[trial],**imshow_kwargs)
-#        ax[num,0].vlines(mean_tau[trial],-0.5,
-#                            mean_lambda.shape[1]-0.5, **vline_kwargs)
-#        ax[num,0].hlines(len(dat.region_units[0]) -0.5 ,**hline_kwargs)
-#        ax[num,1].vlines(mean_tau_stft[trial],
-#                            -0.5,stft_cut.shape[1]-0.5,**vline_kwargs)
-#        ax[num,1].set_xticks(stft_tick_inds)
-#        ax[num,1].set_xticklabels(stft_ticks[stft_tick_inds],rotation='vertical')
-#
-#        for state in range(tau_samples.shape[-1]):
-#            ax[num,-1].hist(tau_samples[:,trial,state], bins = 100, density = True)
-#    ax[-1,0].set_xticks(binned_tick_inds)
-#    ax[-1,0].set_xticklabels(binned_tick_vals, rotation = 'vertical')
-#    ax[-1,-1].set_xticks(binned_tick_inds)
-#    ax[-1,-1].set_xticklabels(binned_tick_vals, rotation = 'vertical')
-#
-#    fig.suptitle(region_unit_count)
-#    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-#            
-#    plt.savefig(os.path.join(\
-#            this_plot_dir,'taste{}_changepoints'.format(this_taste)),dpi=300)
-#    plt.close(fig)
-
 
 ##################################################
 # Good Trial Changepoint Plot
@@ -280,10 +272,10 @@ good_trial_list = np.where([all(x[np.triu_indices(states-1,1)] < lower_thresh) \
 tick_interval = 5
 max_trials = 15
 num_plots = int(np.ceil(len(good_trial_list)/max_trials))
-trial_inds_list = [good_trial_list[x*max_trials:(x+1)*max_trials] \
+good_trial_inds_list = [good_trial_list[x*max_trials:(x+1)*max_trials] \
                         for x in np.arange(num_plots)]
 
-create_plots(plot_spikes, stft_cut, tau_samples, trial_inds_list,
+create_changepoint_plots(plot_spikes, stft_cut, tau_samples, good_trial_inds_list,
             region_unit_count, taste_label, dat.region_units,
             stft_ticks, stft_tick_inds,
             binned_tick_inds, binned_tick_vals)
@@ -294,90 +286,12 @@ for fig_num in tqdm(plt.get_fignums()):
             this_plot_dir,'good_changepoints{}'.format(fig_num)),dpi=300)
     plt.close(fig_num)
                 
-
-def create_plots(spike_array, stft_array, tau_samples, trial_inds_list, 
-                    suptitle, taste_label, region_units_list, 
-                    stft_ticks, stft_tick_inds, 
-                    binned_tick_inds, binned_tick_vals,
-                    plot_type = 'raster'):
-
-    mean_tau = np.mean(tau_samples,axis=0)
-    mean_tau_stft = (mean_tau/spike_array.shape[-1])*stft_cut.shape[-1]
-
-    fig_list = []
-    for fig_num in tqdm(np.arange(len(trial_inds_list))):
-        trial_inds = trial_inds_list[fig_num]
-        trial_count = len(trial_inds)
-        
-        fig, ax = plt.subplots(trial_count,3,sharex='col', 
-                            figsize = (20,trial_count*3))
-        for num,trial in enumerate(trial_inds):
-            if plot_type == 'heatmap':
-                ax[num,0].imshow(spike_array[trial], **imshow_kwargs)
-            elif plot_type == 'raster':
-                ax[num,0].scatter(*np.where(spike_array[trial])[::-1], marker = "|")
-            ax[num,0].set_ylabel(taste_label[trial])
-            ax[num,1].imshow(stft_array[trial], **imshow_kwargs)
-            ax[num,0].hlines(len(region_units_list[0]) -0.5 ,**hline_kwargs)
-            ax[num,0].vlines(mean_tau[trial],-0.5,spike_array.shape[1]-0.5,
-                                **vline_kwargs)
-            ax[num,1].vlines(mean_tau_stft[trial],-0.5,stft_cut.shape[1]-0.5,
-                                **vline_kwargs)
-
-            ax[num,1].set_xticks(stft_tick_inds)
-            ax[num,1].set_xticklabels(stft_ticks[stft_tick_inds],rotation='vertical')
-
-            for state in range(tau_samples.shape[-1]):
-                ax[num,-1].hist(tau_samples[:,trial,state], 
-                    bins = 100, density = True)
-        ax[-1,0].set_xticks(binned_tick_inds)
-        ax[-1,0].set_xticklabels(binned_tick_vals, rotation = 'vertical')
-        ax[-1,-1].set_xticks(binned_tick_inds)
-        ax[-1,-1].set_xticklabels(binned_tick_vals, rotation = 'vertical')
-
-        fig.suptitle(suptitle)
-        fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-        fig_list.append(fig.copy())
-
-#for fig_num in np.arange(num_plots):
-#    trial_inds = trial_inds_list[fig_num]
-#    trial_count = len(trial_inds)
-#    
-#    fig, ax = plt.subplots(trial_count,3,sharex='col', figsize = (20,trial_count*3))
-#    for num,trial in enumerate(trial_inds):
-#        #ax[num,0].imshow(plot_spikes[trial], **imshow_kwargs)
-#        ax[num,0].scatter(*np.where(plot_spikes[trial])[::-1], marker = "|")
-#        ax[num,0].set_ylabel(taste_label[trial])
-#        ax[num,1].imshow(stft_cut[trial], **imshow_kwargs)
-#        ax[num,0].hlines(len(dat.region_units[0]) -0.5 ,**hline_kwargs)
-#        ax[num,0].vlines(mean_tau[trial],-0.5,mean_lambda.shape[1]-0.5,
-#                            **vline_kwargs)
-#        ax[num,1].vlines(mean_tau_stft[trial],-0.5,stft_cut.shape[1]-0.5,
-#                            **vline_kwargs)
-#
-#        ax[num,1].set_xticks(stft_tick_inds)
-#        ax[num,1].set_xticklabels(stft_ticks[stft_tick_inds],rotation='vertical')
-#
-#        for state in range(tau_samples.shape[-1]):
-#            ax[num,-1].hist(tau_samples[:,trial,state], bins = 100, density = True)
-#    ax[-1,0].set_xticks(binned_tick_inds)
-#    ax[-1,0].set_xticklabels(binned_tick_vals, rotation = 'vertical')
-#    ax[-1,-1].set_xticks(binned_tick_inds)
-#    ax[-1,-1].set_xticklabels(binned_tick_vals, rotation = 'vertical')
-#
-#    fig.suptitle(region_unit_count)
-#    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-#
-#    plt.savefig(os.path.join(\
-#            this_plot_dir,'good_changepoints{}'.format(fig_num)),dpi=300)
-#    plt.close(fig)
-
 ##################################################
 # Good Trial Changepoint Plot - Color
 ##################################################
 imshow_kwargs = {'interpolation':'nearest','aspect':'auto','origin':'lower'}
 
-create_plots(binned_dat, stft_cut, tau_samples, trial_inds_list,
+create_changepoint_plots(binned_dat, stft_cut, tau_samples, good_trial_inds_list,
             region_unit_count, taste_label, dat.region_units,
             stft_ticks, stft_tick_inds,
             binned_tick_inds, binned_tick_vals, plot_type = 'heatmap')
@@ -387,37 +301,50 @@ for fig_num in tqdm(plt.get_fignums()):
     plt.savefig(os.path.join(\
             this_plot_dir,'good_changepoints_color{}'.format(fig_num)),dpi=300)
     plt.close(fig_num)
-                
 
-#for fig_num in np.arange(num_plots):
-#    trial_inds = trial_inds_list[fig_num]
-#    trial_count = len(trial_inds)
-#    
-#    fig, ax = plt.subplots(trial_count,3,sharex='col', figsize = (20,trial_count*3))
-#    for num,trial in enumerate(trial_inds):
-#        ax[num,0].imshow(binned_dat[trial], cmap='jet',**imshow_kwargs)
-#        #ax[num,0].scatter(*np.where(plot_spikes[trial])[::-1], marker = "|")
-#        ax[num,0].set_ylabel(taste_label[trial])
-#        ax[num,1].imshow(stft_cut[trial], **imshow_kwargs)
-#        ax[num,0].hlines(len(dat.region_units[0]) -0.5 ,**hline_kwargs)
-#        ax[num,0].vlines(mean_tau[trial],-0.5,mean_lambda.shape[1]-0.5,
-#                            **vline_kwargs)
-#        ax[num,1].vlines(mean_tau_stft[trial],-0.5,stft_cut.shape[1]-0.5,
-#                            **vline_kwargs)
-#
-#        ax[num,1].set_xticks(stft_tick_inds)
-#        ax[num,1].set_xticklabels(stft_ticks[stft_tick_inds],rotation='vertical')
-#
-#        for state in range(tau_samples.shape[-1]):
-#            ax[num,-1].hist(tau_samples[:,trial,state], bins = 100, density = True)
-#    ax[-1,0].set_xticks(binned_tick_inds)
-#    ax[-1,0].set_xticklabels(binned_tick_vals, rotation = 'vertical')
-#    ax[-1,-1].set_xticks(binned_tick_inds)
-#    ax[-1,-1].set_xticklabels(binned_tick_vals, rotation = 'vertical')
-#
-#    fig.suptitle(region_unit_count)
-#    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-#
-#    plt.savefig(os.path.join(\
-#            this_plot_dir,'good_changepoints{}_color'.format(fig_num)),dpi=300)
-#    plt.close(fig)
+##################################################
+# Changepoint plots on RAW spikes 
+##################################################
+# Create same plots as above on unbinned data
+cut_spikes = np.array(dat.spikes)[...,time_lims[0]:time_lims[1]]
+spike_array_long = np.reshape(cut_spikes,(-1,*cut_spikes.shape[-2:]))
+raw_tick_inds = np.arange(0,cut_spikes.shape[-1],
+                        cut_spikes.shape[-1]//time_tick_count)
+scaled_tau_samples = (tau_samples/binned_dat.shape[-1])*cut_spikes.shape[-1]
+
+this_plot_dir = os.path.join(plot_dir, 'full_spike_changepoints')
+if not os.path.exists(this_plot_dir):
+    os.makedirs(this_plot_dir)
+
+create_changepoint_plots(spike_array_long, stft_cut, scaled_tau_samples, 
+            full_trial_inds_list,
+            region_unit_count, taste_label, dat.region_units,
+            stft_ticks, stft_tick_inds,
+            raw_tick_inds, binned_tick_vals)
+
+for fig_num in tqdm(plt.get_fignums()):
+    plt.figure(fig_num)
+    plt.savefig(os.path.join(\
+            this_plot_dir,'full_spike_changepoints{}'.format(fig_num)),dpi=300)
+    plt.close(fig_num)
+
+########################################
+
+this_plot_dir = os.path.join(plot_dir, 'full_spike_good_changepoints')
+if not os.path.exists(this_plot_dir):
+    os.makedirs(this_plot_dir)
+
+create_changepoint_plots(spike_array_long, stft_cut, scaled_tau_samples, 
+            good_trial_inds_list,
+            region_unit_count, taste_label, dat.region_units,
+            stft_ticks, stft_tick_inds,
+            raw_tick_inds, binned_tick_vals)
+
+for fig_num in tqdm(plt.get_fignums()):
+    plt.figure(fig_num)
+    plt.savefig(os.path.join(\
+            this_plot_dir,'full_spike_good_changepoints{}'.format(fig_num)),dpi=300)
+    plt.close(fig_num)
+
+## ** NO POINT PLOTTING FULL SPIKES AS HEATMAP
+## ** SPIKES DON'T SHOW UP IN IMAGE
