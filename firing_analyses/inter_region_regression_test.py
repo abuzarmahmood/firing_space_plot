c2fef585ecc0628984635886e994665d906c7141
abuzarmahmood <abuzarmahmood@gmail.com>
2021-04-03 22:42:57 -0400

    [FEATURE] Add parallelization over repeats

--- firing_analyses/inter_region_regression_test.py
+++ firing_analyses/inter_region_regression_test.py
@@ -22,6 +22,7 @@ from sklearn.model_selection import cross_val_predict, cross_val_score
 from sklearn.linear_model import LinearRegression
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.model_selection import GroupKFold, GroupShuffleSplit
+from joblib import Parallel, delayed, cpu_count
 
 vector_percentile = lambda dist,vals: np.array(\
         list(map(lambda vals: percentileofscore(dist, vals), vals)))
@@ -62,7 +63,6 @@ def trial_shuffle_gen(label_list):
     new_trial_order = np.random.permutation(np.unique(label_list))
     return np.concatenate([np.where(label_list == x)[0] for x in new_trial_order])
 
-split_repeat_num = 10
 
 ###################################################
 # ____                              _             
@@ -77,96 +77,170 @@ split_repeat_num = 10
 ## Perform regression for all trials collectively
 ##################################################
 ## No crossvalidation
+def run_regression(firing_array,trial_labels, shuffle_repeats = 100 
+        cv_splitter = None, scoring_metric = None, cv_bool = False):
+        """
+        firing_array : 2D : (trials x time_bins) x nrns
+        cv_bool : use cross validation?
+        """
+        if cv_bool:
+            if cv_splitter == None or scoring_metric == None:
+                raise Exception('cv_splitter or scoring_metric not provided')
 
-all_percentiles = []
-for this_split in trange(split_repeat_num):
-    grp1,grp2 = np.array_split(\
-            np.random.permutation(np.arange(firing_long_scaled.shape[1])),2)
+        grp1,grp2 = np.array_split(\
+                np.random.permutation(np.arange(firing_long_scaled.shape[1])),2)
 
-    X,y = firing_long_scaled[:,grp1], firing_long_scaled[:,grp2]
-    reg = LinearRegression().fit(X, y)
-    actual_score = r2_score(y,reg.predict(X))
+        X,y = firing_long_scaled[:,grp1], firing_long_scaled[:,grp2]
 
-    # Shuffled trials
-    repeats = 100
-    shuffled_scores = []
-    for repeat in range(repeats):
+        if cv_bool:
+            lm = LinearRegression()
+            cv_iter = cv_splitter.split(X, y, trial_labels)
+            actual_score = cross_val_score(lm, X, y, 
+                    cv=cv_iter, scoring_metric = metric)
+        else:
+            reg = LinearRegression().fit(X, y)
+            actual_score = r2_score(y,reg.predict(X))
 
-        X_sh = X[trial_shuffle_gen(trial_labels)] 
-        y_sh = y[trial_shuffle_gen(trial_labels)]
+        # Shuffled trials
+        shuffled_scores = []
+        for repeat in range(shuffle_repeats):
 
-        reg = LinearRegression().fit(X_sh, y_sh)
-        shuffled_scores.append(r2_score(y_sh,reg.predict(X_sh)))
+            X_sh = X[trial_shuffle_gen(trial_labels)] 
+            y_sh = y[trial_shuffle_gen(trial_labels)]
 
-    actual_data_percentile = percentileofscore(shuffled_scores, actual_score)
-    all_percentiles.append(actual_data_percentile)
+            if cv_bool:
+                lm = LinearRegression()
+                cv_iter = cv_splitter.split(X_sh, y_sh, trial_labels)
+                shuffled_scores.append(cross_val_score(lm, X_sh, y_sh, 
+                    cv=cv_iter, scoring_metric = metric))
+            else:
+                reg = LinearRegression().fit(X_sh, y_sh)
+                shuffled_scores.append(r2_score(y_sh,reg.predict(X_sh)))
 
-plt.hist(shuffled_scores,bins=50)
-plt.axvline(actual_score)
-plt.title(f'Percentile : {actual_data_percentile}')
-plt.show()
+        if not cv_bool:
+            shuffled_scores = [shuffled_scores]
+            actual_score = [actual_score]
+        actual_data_percentile = \
+                vector_percentile(np.concatenate(shuffled_scores), actual_score)
 
-##################################################
-## With cross-validation
-##################################################
-## K-Fold cross validation will hold out chunks in CONTINUOUS TIME
-## There is no guarantee that the activity of the populations will be stable
-## across the session. This means that the relationship between the populations
-## could be different on the trained data and the validation data
-## To mitigate this issue:
-## 1) We could train and test on smaller chunks of data (e.g. 10 trial blocks)
-## 2) Use GroupKFold or GroupShuffleSplit (with groups as trials) to
-##      sample adequately from each time period
-# https://scikit-learn.org/stable/auto_examples/model_selection/plot_cv_indices.html
+        return actual_data_percentile, actual_score, shuffled_scores
 
-cv_split = 10
-shuffle_repeats = 100
+def parallelize(func, iterator):
+    return Parallel(n_jobs = cpu_count()-2)\
+            (delayed(func)(this_iter) for this_iter in tqdm(iterator))
+
 split_repeat_num = 30
-metric = 'r2'
-all_percentiles = []
-
 gss = GroupShuffleSplit(n_splits=cv_split, train_size=.9)
 
-#all_inds = []
-#for train_idx, test_idx in gss.split(X, y, trial_labels):
-#    all_inds.append(train_idx)
-#ind_mat = np.zeros((cv_split,len(trial_labels)))
-#for num,this_inds in enumerate(all_inds):
-#    ind_mat[num,this_inds] = 1
-#visualize.imshow(ind_mat)
-#plt.show()
+this_func = lambda x : run_regression(firing_long_scaled, trial_labels,
+                cv_splitter = gss, scoring_metric = metric, cv_bool = False)
+outs = parallelize(this_func, range(split_repeat_num))
+actual_data_percentile, actual_score, shuffled_scores = list(zip(*outs))
+actual_data_percentile = np.concatenate(actual_data_percentile)
 
-for this_split in trange(split_repeat_num):
-
-    # Actual data
-    grp1,grp2 = np.array_split(\
-            np.random.permutation(np.arange(firing_long_scaled.shape[1])),2)
-
-    X,y = firing_long_scaled[:,grp1], firing_long_scaled[:,grp2]
-
-    lm = LinearRegression()
-    cv_iter = gss.split(X, y, trial_labels)
-    actual_score = cross_val_score(lm, X, y, cv=cv_iter, scoring = metric)
-
-    # Shuffled trials
-    shuffled_scores = []
-    for repeat in range(shuffle_repeats):
-
-        X_sh = X[trial_shuffle_gen(trial_labels)] 
-        y_sh = y[trial_shuffle_gen(trial_labels)]
-
-        lm = LinearRegression()
-        cv_iter = gss.split(X_sh, y_sh, trial_labels)
-        shuffled_scores.append(cross_val_score(lm, X_sh, y_sh, cv=cv_iter, 
-            scoring = metric))
-
-    actual_data_percentile = vector_percentile(np.concatenate(shuffled_scores), 
-                                            actual_score)
-
-    all_percentiles.append(actual_data_percentile)
+# With cross-validation
+this_func = lambda x : run_regression(firing_long_scaled, trial_labels,
+                cv_splitter = gss, scoring_metric = metric, cv_bool = True)
+outs = parallelize(this_func, range(split_repeat_num))
+actual_data_percentile, actual_score, shuffled_scores = list(zip(*outs))
+actual_data_percentile = np.concatenate(actual_data_percentile)
 
 vals,bins,patches = \
         plt.hist(np.concatenate(all_percentiles), bins = np.linspace(0,100,21))
 patches[-1].set_fc('red')
 plt.show()
 
+#all_percentiles = []
+#for this_split in trange(split_repeat_num):
+#    grp1,grp2 = np.array_split(\
+#            np.random.permutation(np.arange(firing_long_scaled.shape[1])),2)
+#
+#    X,y = firing_long_scaled[:,grp1], firing_long_scaled[:,grp2]
+#    reg = LinearRegression().fit(X, y)
+#    actual_score = r2_score(y,reg.predict(X))
+#
+#    # Shuffled trials
+#    repeats = 100
+#    shuffled_scores = []
+#    for repeat in range(repeats):
+#
+#        X_sh = X[trial_shuffle_gen(trial_labels)] 
+#        y_sh = y[trial_shuffle_gen(trial_labels)]
+#
+#        reg = LinearRegression().fit(X_sh, y_sh)
+#        shuffled_scores.append(r2_score(y_sh,reg.predict(X_sh)))
+#
+#    actual_data_percentile = percentileofscore(shuffled_scores, actual_score)
+#    all_percentiles.append(actual_data_percentile)
+#
+#plt.hist(shuffled_scores,bins=50)
+#plt.axvline(actual_score)
+#plt.title(f'Percentile : {actual_data_percentile}')
+#plt.show()
+#
+###################################################
+### With cross-validation
+###################################################
+### K-Fold cross validation will hold out chunks in CONTINUOUS TIME
+### There is no guarantee that the activity of the populations will be stable
+### across the session. This means that the relationship between the populations
+### could be different on the trained data and the validation data
+### To mitigate this issue:
+### 1) We could train and test on smaller chunks of data (e.g. 10 trial blocks)
+### 2) Use GroupKFold or GroupShuffleSplit (with groups as trials) to
+###      sample adequately from each time period
+## https://scikit-learn.org/stable/auto_examples/model_selection/plot_cv_indices.html
+#
+#cv_split = 10
+#shuffle_repeats = 100
+#split_repeat_num = 30
+#metric = 'r2'
+#all_percentiles = []
+#
+#gss = GroupShuffleSplit(n_splits=cv_split, train_size=.9)
+#
+##all_inds = []
+##for train_idx, test_idx in gss.split(X, y, trial_labels):
+##    all_inds.append(train_idx)
+##ind_mat = np.zeros((cv_split,len(trial_labels)))
+##for num,this_inds in enumerate(all_inds):
+##    ind_mat[num,this_inds] = 1
+##visualize.imshow(ind_mat)
+##plt.show()
+#
+#
+#
+#for this_split in trange(split_repeat_num):
+#
+#    # Actual data
+#    grp1,grp2 = np.array_split(\
+#            np.random.permutation(np.arange(firing_long_scaled.shape[1])),2)
+#
+#    X,y = firing_long_scaled[:,grp1], firing_long_scaled[:,grp2]
+#
+#    lm = LinearRegression()
+#    cv_iter = gss.split(X, y, trial_labels)
+#    actual_score = cross_val_score(lm, X, y, cv=cv_iter, scoring = metric)
+#
+#    # Shuffled trials
+#    shuffled_scores = []
+#    for repeat in range(shuffle_repeats):
+#
+#        X_sh = X[trial_shuffle_gen(trial_labels)] 
+#        y_sh = y[trial_shuffle_gen(trial_labels)]
+#
+#        lm = LinearRegression()
+#        cv_iter = gss.split(X_sh, y_sh, trial_labels)
+#        shuffled_scores.append(cross_val_score(lm, X_sh, y_sh, cv=cv_iter, 
+#            scoring = metric))
+#
+#    actual_data_percentile = \
+#            vector_percentile(np.concatenate(shuffled_scores), actual_score)
+#
+#    all_percentiles.append(actual_data_percentile)
+#
+#vals,bins,patches = \
+#        plt.hist(np.concatenate(all_percentiles), bins = np.linspace(0,100,21))
+#patches[-1].set_fc('red')
+#plt.show()
+#
