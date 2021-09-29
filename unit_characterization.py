8e4461c558f70a2052c9788223472b8392f15529
abuzarmahmood <abuzarmahmood@gmail.com>
2021-09-29 13:34:16 -0400

    Add taste discriminability analysis

--- unit_characterization.py
+++ unit_characterization.py
@@ -33,7 +33,13 @@ from joblib import Parallel, delayed, cpu_count
 import itertools as it
 import ast
 from scipy.stats import spearmanr, percentileofscore, chisquare, ttest_rel
+from scipy.stats import kruskal
 import pylab as plt
+from glob import glob
+import json
+from scipy.spatial import distance_matrix as distmat
+from sklearn.preprocessing import LabelEncoder as LE
+import pingouin as pg
 
 sys.path.append('/media/bigdata/firing_space_plot/ephys_data')
 from ephys_data import ephys_data
@@ -51,7 +57,9 @@ def isi_rate(array, smoothing_fraction = 0.05):
     spike_inds = [np.where(array[x])[0] for x in trial_inds]
     isi_s = [np.diff(inds) for inds in spike_inds]
     spike_counts = [len(x) for x in spike_inds]
-    kern_len = [int(smoothing_fraction * count) for count in spike_counts]
+    kern_len = np.array([int(smoothing_fraction * count) for count in spike_counts])
+    # Enforce minimum length of 1
+    kern_len[np.where(kern_len == 0)] = 1
     filt_isi = [np.convolve(x, np.ones(this_len), mode = 'same')\
             if len(x) > this_len else np.ones(len(x))*np.inf \
             for x, this_len in zip(isi_s, kern_len)]
@@ -77,7 +85,11 @@ def isi_rate(array, smoothing_fraction = 0.05):
 
 dir_list_path = "/media/bigdata/Abuzar_Data/dir_list.txt"
 dir_list = [x.strip() for x in open(dir_list_path,'r').readlines()]
-data_dir = dir_list[-1]
+#data_dir = dir_list[39]
+
+#for data_dir in tqdm(dir_list):
+
+data_dir = dir_list[0]
 
 dat = ephys_data(data_dir)
 dat.get_spikes()
@@ -88,26 +100,17 @@ region_frame = pd.concat(\
                         'neuron' : nrns}) \
                 for region, nrns in zip(dat.region_names, dat.region_units)])
 
-spikes = np.array(dat.spikes).swapaxes(1,2).swapaxes(0,1)
+dat.check_laser()
+if dat.laser_exists:
+    dat.separate_laser_spikes()
+    spikes = np.array(dat.off_spikes).swapaxes(1,2).swapaxes(0,1)
+else:
+    spikes = np.array(dat.spikes).swapaxes(1,2).swapaxes(0,1)
 
-#########################################
-### Responsive Neurons 
-#########################################
-#base_time_lims = [0,2000]
-#post_time_lims = [2000, 4000]
-#
-#base_spikes = np.mean(spikes[..., base_time_lims[0]:base_time_lims[1]],axis=-1)
-#post_spikes = np.mean(spikes[..., post_time_lims[0]:post_time_lims[1]],axis=-1)
-#
-## Run pairwise t-tests across trials
-#inds = list(np.ndindex(base_spikes.shape[:-1]))
-#p_vals_array = np.empty(base_spikes.shape[:-1])
-#for this_ind in inds:
-#    p_vals_array[this_ind] = ttest_rel(base_spikes[this_ind],
-#                                        post_spikes[this_ind])[1]
-#alpha = 0.05
-#sig_array = p_vals_array < alpha
-#neuron_ind, taste_ind = np.where(sig_array)
+sum_spikes = np.sum(spikes,axis=(1,2,3))
+# Put one spike at start and end to ensure there is atleast 1 ISI
+spikes[...,0] = 1
+spikes[...,-1] = 1
 
 ########################################
 ## PSTH Repsonses
@@ -116,14 +119,7 @@ spikes = np.array(dat.spikes).swapaxes(1,2).swapaxes(0,1)
 psths = np.sum(spikes, axis=2)
 
 step_size = 0.001
-#firing_array = dat._calc_baks_rate(step_size, 0.001, psths)
 x_new, firing_array = isi_rate(psths)
-
-#sig_spikes = np.array([spikes[this_ind] for this_ind \
-#        in list(zip(*np.where(sig_array)))])
-#sig_sum_spikes = np.sum(sig_spikes,axis=1)
-#sig_responses = np.array([firing_array[this_ind] for this_ind \
-#        in list(zip(*np.where(sig_array)))])
 
 ########################################
 # Response period
@@ -138,6 +134,7 @@ pre_stim_rates = cut_firing[...,:(stim_ind-250)]
 post_stim_rates = cut_firing[...,stim_ind:]
 
 inds = list(np.ndindex(pre_stim_rates.shape[:-1]))
+firing_long = np.array([cut_firing[this_ind] for this_ind in inds])
 pre_stim_long = np.array([pre_stim_rates[this_ind] for this_ind in inds])
 post_stim_long = np.array([post_stim_rates[this_ind] for this_ind in inds])
 
@@ -149,7 +146,8 @@ ci_sig_array = np.array([(this_post < this_ci[0])*1 + (this_post > this_ci[1]) \
         for this_post, this_ci in zip(post_stim_long, pre_stim_ci)])
 
 sig_window = 150
-this_conv = lambda x : np.convolve(x, np.ones(sig_window)/sig_window, mode = 'same')
+this_conv = lambda x : np.convolve(
+        x, np.ones(sig_window)/sig_window, mode = 'same')
 # Ensuring the selection criteria for the response is true
 sig_conv = np.apply_along_axis(this_conv, axis=-1, arr = ci_sig_array) 
 
@@ -192,15 +190,19 @@ fin_frame = fin_frame.merge(nrn_ind_frame, how = 'inner', on = 'response')
 fin_frame.drop(labels = ['off','on'], axis = 1, inplace=True)
 
 # Max out values at 2000 post-stim
-fin_frame[['time_on','time_off']] = fin_frame[['time_on','time_off']].clip(upper = 2000)
+post_upper_lim = 5000
+fin_frame[['time_on','time_off']] = \
+        fin_frame[['time_on','time_off']].clip(upper = post_upper_lim)
 fin_frame['duration'] = fin_frame['time_off'] - fin_frame['time_on']
 fin_frame = fin_frame[[
         'neuron','taste','response','region','time_on','time_off','duration']]
 
+fin_frame.to_hdf(dat.hdf5_path, 
+        key = os.path.join('ancillary_analysis', 'unit_response_characteristic'))
+
 first_up = fin_frame.time_on
 first_down = fin_frame.time_off
-sig_conv = sig_conv[np.sort(fin_frame.response)]
-ci_sig_array = ci_sig_array[np.sort(fin_frame.response)]
+response_num = fin_frame.response
 
 mean_nrn_frame = fin_frame.groupby('neuron').mean().reset_index()
 mean_nrn_frame = mean_nrn_frame.merge(\
@@ -209,6 +211,15 @@ mean_nrn_frame = mean_nrn_frame.merge(\
                 columns = ['neuron','taste','response'])
 mean_nrn_frame = mean_nrn_frame.round()
 
+########################################
+# ____  _       _       
+#|  _ \| | ___ | |_ ___ 
+#| |_) | |/ _ \| __/ __|
+#|  __/| | (_) | |_\__ \
+#|_|   |_|\___/ \__|___/
+#                       
+########################################
+
 plot_dir = os.path.join(dat.data_dir, 'unit_response_plots')
 if not os.path.exists(plot_dir):
     os.makedirs(plot_dir)
@@ -216,6 +227,8 @@ if not os.path.exists(plot_dir):
 g = sns.relplot(x='duration', y='time_on', col='region', data = fin_frame)
 plt.subplots_adjust(0.1,0.1, top = 0.8)
 plt.suptitle(f'{dat.hdf5_name}' + '\n' 'All responses')
+plt.xlim([0,post_upper_lim])
+plt.ylim([0,post_upper_lim])
 fig = plt.gcf()
 fig.savefig(os.path.join(plot_dir,'all_responses'))
 #plt.show()
@@ -223,63 +236,226 @@ fig.savefig(os.path.join(plot_dir,'all_responses'))
 g = sns.relplot(x='duration', y='time_on', col='region',data = mean_nrn_frame)
 plt.subplots_adjust(0.1,0.1, top = 0.8)
 plt.suptitle(f'{dat.hdf5_name}' + '\n' 'Mean responses')
+plt.xlim([0,post_upper_lim])
+plt.ylim([0,post_upper_lim])
 fig = plt.gcf()
 fig.savefig(os.path.join(plot_dir,'mean_responses'))
-plt.show()
-
-fin_frame.to_hdf(dat.hdf5_path, 
-        key = os.path.join('ancillary_analysis', 'unit_response_characteristic'))
-
-#first_up = np.array([np.where(x==1)[0][0] for x in sig_conv])
-#all_downs = np.array([np.where(x == 0)[0] for up,x in zip(first_up, sig_conv)])
-#first_down = np.array([this_downs[np.where(this_downs > this_up)[0][0]] \
-#        for this_up, this_downs in zip(first_up, all_downs)])
-
-#fig,ax = plt.subplots(1,2, sharex=True, sharey=True)
-#ax[0].imshow(sig_conv, aspect='auto')
-#ax[1].imshow(ci_sig_array, aspect='auto')
-#for num, (up, down) in enumerate(zip(first_up, first_down)):
-#    ax[0].scatter(up, num, color = 'red')
-#    ax[0].scatter(down, num, color = 'red')
-#    ax[1].scatter(up, num, color = 'red')
-#    ax[1].scatter(down, num, color = 'red')
 #plt.show()
 
-#durations = first_down-first_up
-#
-#dframe = pd.DataFrame({
-#            'taste' : taste_ind,
-#            'neuron' : neuron_ind,
-#            'up' : first_up,
-#            'duration' : durations})
-#
-#fin_frame = dframe.merge(region_frame, on = 'neuron')
-#fin_frame['session_name'] = os.path.basename(dat.data_dir) 
-#fin_frame['animal_num'] = fin_frame['session_name'].iloc[0].split('_')[0]
-#fin_frame['date'] = fin_frame['session_name'].iloc[0].split('_')[2]
-#fin_frame.sort_values(['neuron','taste'], inplace=True)
-#
-#sort_inds = np.argsort(fin_frame['neuron'].to_numpy())
-#
-#plot_frame = fin_frame.query('region == "bla"')
-#plt.scatter(plot_frame['duration'], plot_frame['up'])
+
+fig,ax = plt.subplots(1,2, sharex=True, sharey=True, figsize = (10,15))
+ax[0].imshow(sig_conv[:,:post_upper_lim], aspect='auto', cmap = 'viridis')
+ax[1].imshow(ci_sig_array[:,:post_upper_lim], aspect='auto')
+for num, up, down in zip(response_num, first_up, first_down):
+    ax[0].scatter(up, num, color = 'red', s=8)
+    ax[0].scatter(down, num, color = 'red', s=8)
+    ax[1].scatter(up, num, color = 'red', s=8)
+    ax[1].scatter(down, num, color = 'red', s=8)
+plt.suptitle(f'{dat.hdf5_name}' + '\n' 'All responses')
+plt.subplots_adjust(0.1,0.1, top = 0.9)
+fig.savefig(os.path.join(plot_dir,'all_responses_up_down'))
 #plt.show()
 
-#fig, ax = plt.subplots(len(sig_conv),1, sharex=True, figsize = (5,15))
-
-#fig,ax = visualize.gen_square_subplots(len(sig_conv))
-#conv_time = np.arange(stim_ind, stim_ind + sig_conv.shape[-1])
-#for num, this_ax in enumerate(ax.flatten()):
-#    #num = sort_inds[in_num]
-#    #zscore_y = stats.zscore(sig_responses[num])
-#    zscore_y = sig_responses[num]
-#    this_ax.plot(zscore_y)
-#    #this_ax.plot(conv_time, sig_conv[num])
-#    times = np.where(sig_conv[num] > 0)[0]+ stim_t
-#    this_ax.scatter(times ,zscore_y[times], s = 5, color = 'orange')
-#    this_ax.axvline(stim_ind, alpha = 0.5, color ='red')
-#    this_ax.axhline(pre_stim_ci[num][0], 0, len(zscore_y), alpha = 0.5, color ='red')
-#    this_ax.axhline(pre_stim_ci[num][1], 0, len(zscore_y), alpha = 0.5, color ='red')
-#    #this_ax.set_ylabel(f'Nrn {fin_frame.neuron.loc[num]}' + '\n' +f'taste {fin_frame.taste.loc[num]}')
+fig,ax = visualize.gen_square_subplots(
+        len(sig_conv), sharex=True, figsize = (20,20))
+conv_time = np.arange(stim_ind, stim_ind + sig_conv.shape[-1])
+for num in range(len(firing_long)):
+    this_ax = ax.flatten()[num]
+    zscore_y = firing_long[num, :(stim_t+post_upper_lim)]
+    this_ax.plot(zscore_y)
+    this_ax.axes.get_yaxis().set_visible(False)
+    this_ax.axvline(stim_ind, alpha = 0.5, color ='red')
+    this_ax.axhline(\
+            pre_stim_ci[num][0], 0, len(zscore_y), alpha = 0.5, color ='red')
+    this_ax.axhline(\
+            pre_stim_ci[num][1], 0, len(zscore_y), alpha = 0.5, color ='red')
+for num, up, down in zip(response_num, first_up, first_down):
+    ax.flatten()[num].\
+            axvspan(up + stim_t ,down + stim_t, color = 'yellow', alpha = 0.5)
+plt.suptitle(f'{dat.hdf5_name}' + '\n' 'All responses' + '\n' + \
+        f'Sig responses {len(response_num)}/{len(sig_conv)}')
+plt.subplots_adjust(0.1,0.1, top = 0.9)
+fig.savefig(os.path.join(plot_dir,'all_responses_traces'))
 #plt.show()
 
+plt.close('all')
+
+############################################################
+#|  _ \ __ _| | __ _| |_ __ _| |__ (_) (_) |_ _   _ 
+#| |_) / _` | |/ _` | __/ _` | '_ \| | | | __| | | |
+#|  __/ (_| | | (_| | || (_| | |_) | | | | |_| |_| |
+#|_|   \__,_|_|\__,_|\__\__,_|_.__/|_|_|_|\__|\__, |
+#                                             |___/ 
+############################################################
+
+json_path = glob(os.path.join(dat.data_dir,"*.info"))[0]
+with open(json_path, 'r') as params_file:
+    info_dict = json.load(params_file)
+tastant_names = info_dict['taste_params']['tastes']
+pal_inds = info_dict['taste_params']['pal_rankings']
+# If recordings don't match tastant order, GTFO: 
+if not pal_inds == [3,4,2,1]: 
+    continue
+
+psth_stim_t = 2000
+post_stim_psths = psths[...,psth_stim_t:]
+epoch_lims = [[0,250],[250,1000],[1000,1750]]
+epoch_stim_rates = np.array([np.mean(post_stim_psths[...,lim[0]:lim[1]],axis=-1)\
+        for lim in epoch_lims])
+inds = np.array(list(np.ndindex(epoch_stim_rates.shape[:-1])))
+epoch_stim_rates_long = [epoch_stim_rates[tuple(this_ind)] for this_ind in inds]
+dist_list = \
+        [distmat(x[:,np.newaxis],x[:,np.newaxis]) for x in epoch_stim_rates_long] 
+# Add a bit of noise to prevent nans
+dist_list = [x + np.random.random(x.shape) * 1e-3 for x in dist_list]
+# Normalize each dist to the max value
+dist_list = [x/np.max(x,axis=None) for x in dist_list]
+similar_dists = [[x[0,1],x[3,2]] for x in dist_list]
+non_similar_dists = [x[2:][:,:2].flatten() for x in dist_list]
+
+similar_frame = pd.DataFrame({
+    'epoch' : np.repeat(inds[:,0],2),
+    'neuron' : np.repeat(inds[:,1],2),
+    'dist' : np.array(similar_dists).flatten(),
+    'type' : 'similar'})
+non_similar_frame = pd.DataFrame({
+    'epoch' : np.repeat(inds[:,0],4),
+    'neuron' : np.repeat(inds[:,1],4),
+    'dist' : np.array(non_similar_dists).flatten(),
+    'type' : 'non_similar'})
+dists_frame = pd.concat([similar_frame, non_similar_frame])
+dists_frame = dists_frame.merge(
+        nrn_ind_frame[['neuron','region']].drop_duplicates(),
+        how = 'inner', on = 'neuron')
+
+dists_frame.region, region_encoding = pd.factorize(dists_frame.region)
+dists_frame.type, type_encoding = pd.factorize(dists_frame.type)
+
+mean_dists_frame = \
+        dists_frame.groupby(['epoch','neuron','type']).mean().reset_index()
+epoch_neuron, grouped_dat = zip(*list(mean_dists_frame.groupby(['epoch','neuron'])))
+epoch_neuron = np.array(epoch_neuron)
+diffs_list = [np.diff(x.dist)[0] for x in grouped_dat]
+region_list  = [x.region.iloc[0] for x in grouped_dat]
+diff_dists_frame = pd.DataFrame({
+    'epoch' : epoch_neuron[:,0],
+    'neuron' : epoch_neuron[:,1],
+    'diff_dist' : diffs_list,
+    'region' : region_list})
+
+mean_dists_array = np.array([x[1].dist for x in mean_dists_frame.groupby('neuron')])
+mean_diff_dists_array = \
+        np.array([x[1].diff_dist for x in diff_dists_frame.groupby('neuron')])
+zscore_diff_dists_array = stats.zscore(mean_diff_dists_array,axis=-1)
+diff_region_list = [x[1].region.iloc[0] for x in diff_dists_frame.groupby('neuron')]
+
+#g = sns.catplot(x = 'epoch', y = 'dist', hue = 'type', col = 'neuron',
+#        kind = 'bar', ci = 'sd', col_wrap = 6, data = dists_frame)
+##plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+#plt.show()
+
+lut = dict(zip(np.unique(diff_region_list),"rbg"))
+row_colors = [lut[x] for x in diff_region_list]
+color_region = [(region_encoding[key],val) for key, val in lut.items()]
+
+if len(dat.region_names) > 1:
+    region_selections = [[0],[1],[0,1]]
+else:
+    region_selections = [[0]]
+
+for this_selection in region_selections:
+    region_inds = [num for num,val in enumerate(diff_region_list) \
+            if val in this_selection]
+    region_names = [region_encoding[x] for x in this_selection]
+
+    if len(region_inds) < 2:
+        open(os.path.join(plot_dir,
+            f'{"_".join(region_names)} doesnt have enough nrns'),'w')
+    else:
+        sns.clustermap(mean_diff_dists_array[region_inds],
+            row_colors = np.array(row_colors)[region_inds], col_cluster=False)
+        plt.subplots_adjust(0.1,0.1, top = 0.9)
+        plt.suptitle(f'Raw dists : {"_".join(region_names)}' + \
+                '\n' + str(color_region))
+        fig = plt.gcf()
+        fig.savefig(os.path.join(plot_dir,f'raw_dists_{"_".join(region_names)}'))
+        #plt.show()
+        sns.clustermap(zscore_diff_dists_array[region_inds],
+            row_colors = np.array(row_colors)[region_inds], col_cluster=False)
+        plt.subplots_adjust(0.1,0.1, top = 0.9)
+        plt.suptitle(f'Zscore dists : {"_".join(region_names)}' + \
+                '\n' + str(color_region))
+        fig = plt.gcf()
+        fig.savefig(os.path.join(
+            plot_dir,f'zscore_dists_{"_".join(region_names)}'))
+        #plt.show()
+
+plt.close('all')
+
+#################################################################################
+# ____  _               _           _             _     _ _ _ _         
+#|  _ \(_)___  ___ _ __(_)_ __ ___ (_)_ __   __ _| |__ (_) (_) |_ _   _ 
+#| | | | / __|/ __| '__| | '_ ` _ \| | '_ \ / _` | '_ \| | | | __| | | |
+#| |_| | \__ \ (__| |  | | | | | | | | | | | (_| | |_) | | | | |_| |_| |
+#|____/|_|___/\___|_|  |_|_| |_| |_|_|_| |_|\__,_|_.__/|_|_|_|\__|\__, |
+#                                                                 |___/ 
+#################################################################################
+
+dat.firing_rate_params = dat.default_firing_params
+dat.firing_rate_params['step_size'] = 50
+dat.firing_rate_params['window_size'] = 500
+dat.get_firing_rates()
+
+firing_array = np.moveaxis(dat.firing_array.swapaxes(0,1),-1,1)
+
+p_val_array = np.empty(firing_array.shape[:2])
+inds = np.array(list(np.ndindex(firing_array.shape[:2])))
+for this_ind in inds:
+    p_val_array[this_ind] = kruskal(*firing_array[this_ind])[1]
+
+stim_t = 2000
+t_vec = np.linspace(0,spikes.shape[-1],firing_array.shape[1]) - stim_t
+
+save_path = '/ancillary_analysis'
+group_name = 'taste_discriminability'
+fin_save_path = os.path.join(save_path, group_name)
+with tables.open_file(dat.hdf5_path,'r+') as h5:
+    if fin_save_path not in h5:
+        h5.create_group(save_path, group_name)
+    h5.create_array(fin_save_path, 'taste_discrim_kw', p_val_array)
+    h5.create_array(fin_save_path, 'post_stim_time', t_vec)
+    h5.flush()
+
+#taste_discrim_frame = pd.DataFrame({
+#        'neuron' : inds[:,0],
+#        'time_bin' : inds[:,1],
+#        'post_stim_t' : np.tile(t_vec, p_val_array.shape[0]),
+#        'p_vals' : p_val_array.flatten()})
+#
+#taste_discrim_frame = \
+#        taste_discrim_frame.merge(
+#                nrn_ind_frame[['neuron','region']].drop_duplicates(),
+#                how = 'inner', on = 'neuron')
+#sns.relplot(x = 'post_stim_t', y = 'p_vals', hue = 'neuron', 
+#        data = taste_discrim_frame, col = 'region', kind = 'line')
+#plt.show()
+
+alpha = 0.05
+p_val_sig_array = p_val_array <= alpha
+
+post_t_lims = [-500,2000]
+wanted_inds = np.array([num for num,x in enumerate(t_vec) \
+        if post_t_lims[0] < x <= post_t_lims[1]])
+cut_p_val_sig_array = p_val_sig_array[...,wanted_inds]
+sorted_sig_arrays = [cut_p_val_sig_array[units] for units in dat.region_units]
+fig,ax = plt.subplots(2,len(dat.region_names), sharex=True)
+for num,this_array in enumerate(sorted_sig_arrays):
+    ax[0,num].pcolormesh(
+            t_vec[wanted_inds], np.arange(this_array.shape[0]), this_array)
+    ax[1,num].plot(t_vec[wanted_inds], np.mean(this_array,axis=0))
+    ax[1,num].set_ylim([0,1])
+    ax[0,num].set_title(dat.region_names[num])
+plt.suptitle(dat.hdf5_name + '\n' + f'Taste ANOVA, alpha = {alpha}')
+plt.subplots_adjust(0.1,0.1, top = 0.8)
+fig.savefig(os.path.join(plot_dir),'discrim_region_plot')
+#plt.show()
