1fe5714dbeeafa93b4f2c9f809929f2099786a10
abuzarmahmood <abuzarmahmood@gmail.com>
2021-11-30 13:58:05 -0500

    First pass at preprocess_all_taste

--- changepoint_mcmc/v2/changepoint_preprocess.py
+++ changepoint_mcmc/v2/changepoint_preprocess.py
@@ -66,4 +66,48 @@ def preprocess_single_taste(spike_array, time_lims, bin_width, data_transform):
     return spike_binned
 
 def preprocess_all_taste():
-    pass
+    """
+    spike_array : trials x neurons x time
+    """
+    accepted_transforms = ['shuffled','simulated','None',None]
+    if data_transform not in accepted_transforms:
+        raise Exception(f'data_transform must be of type {accepted_transforms}')
+
+    ##################################################
+    ## Create shuffled data
+    ##################################################
+    # Shuffle neurons across trials FOR SAME TASTE
+
+    if data_transform == 'shuffled':
+        transformed_dat = np.array([np.random.permutation(neuron) \
+                    for neuron in np.swapaxes(spike_array,1,0)])
+        transformed_dat = np.swapaxes(transformed_dat,0,1)
+
+    ##################################################
+    ## Create simulated data 
+    ##################################################
+    # Inhomogeneous poisson process using mean firing rates
+
+    elif data_transform == 'simulated':
+        mean_firing = np.mean(spike_array,axis=0)
+
+        # Simulate spikes
+        transformed_dat = np.array(\
+                [np.random.random(mean_firing.shape) < mean_firing \
+                for trial in range(spike_array.shape[0])])*1
+
+    ##################################################
+    ## Null Transform Case 
+    ##################################################
+    elif data_transform is None:
+        transformed_dat = spike_array
+
+    ##################################################
+    ## Bin Data 
+    ##################################################
+    this_dat_binned = \
+            np.sum(taste_dat[...,time_lims[0]:time_lims[1]].\
+            reshape(*taste_dat.shape[:-1],-1,bin_width),axis=-1)
+    this_dat_binned = np.vectorize(np.int)(this_dat_binned)
+
+    return spike_binned
