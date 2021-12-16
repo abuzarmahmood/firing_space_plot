ed2185814ff4c638ae178cd674e778082db1b5cb
Abuzar Mahmood <abuzarmahmood@gmail.com>
2021-12-16 00:19:18 -0500

    Update changepoint_analysis.py to have accurate imports and definitions

--- changepoint_mcmc/v2/changepoint_analysis.py
+++ changepoint_mcmc/v2/changepoint_analysis.py
@@ -1,22 +1,24 @@
+#sys.path.append('/media/bigdata/firing_space_plot/ephys_data')
+#sys.path.append('/media/bigdata/firing_space_plot/changepoint_mcmc/v2')
+#from ephys_data import ephys_data
+#from changepoint_io import fit_handler
+#import itertools as it
+#from tqdm import tqdm
+#import ast
+
 import sys
-sys.path.append('/media/bigdata/firing_space_plot/ephys_data')
-sys.path.append('/media/bigdata/firing_space_plot/changepoint_mcmc/v2')
-from ephys_data import ephys_data
-from changepoint_io import fit_handler
-import itertools as it
-from tqdm import tqdm
 import os
 import pandas as pd
 import numpy as np
-import ast
 import pickle as pkl
+
 
 FIT_PKL = '/media/bigdata/firing_space_plot/changepoint_mcmc/'\
         'saved_models/natasha_gc_binary/natasha_gc_binary_0296f33c.info'
 
-dir_name = os.path.dirname(FIT_PKL)
-file_name = os.path.basename(FIT_PKL)
-file_name_base = file_name.split('.')[0]
+#dir_name = os.path.dirname(FIT_PKL)
+#file_name = os.path.basename(FIT_PKL)
+#file_name_base = file_name.split('.')[0]
 
 class pkl_handler():
     def __init__(self, file_path):
