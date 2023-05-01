f56ec45222da39ce61a70dc83a4d51c3ed4ab783
abuzarmahmood <abuzarmahmood@gmail.com>
2023-04-30 22:35:10 -0400

    Update to calculate granger causality for time windows

--- lfp_analyses/granger_causality/single_trial_granger_causality.py
+++ lfp_analyses/granger_causality/single_trial_granger_causality.py
@@ -24,6 +24,9 @@ from statsmodels.tsa.stattools import adfuller, kpss, grangercausalitytests
 from statsmodels.tsa.vector_ar.var_model import VAR
 from joblib import Parallel, delayed
 from tqdm import tqdm
+import pandas as pd
+import pingouin as pg
+import seaborn as sns
 
 def parallelize(func, arg_list, n_jobs=10):
     return Parallel(n_jobs=n_jobs)(delayed(func)(arg) for arg in tqdm(arg_list))
@@ -48,6 +51,8 @@ h5_path = glob(dir_name + '/*.h5')[0]
 print(f'Processing {basename}')
 
 dat = ephys_data(dir_name)
+dat.get_info_dict()
+taste_names = dat.info_dict['taste_params']['tastes']
 # Region lfps shape : (n_tastes, n_channels, n_trials, n_timepoints)
 lfp_channel_inds, region_lfps, region_names = \
     dat.return_representative_lfp_channels()
@@ -61,6 +66,10 @@ flat_region_lfps = np.reshape(
 
 # 1) Remove trials with artifacts
 #good_lfp_trials_bool = dat.lfp_processing.return_good_lfp_trial_inds(dat.all_lfp_array)
+trial_labels = np.repeat(np.arange(region_lfps.shape[1]), region_lfps.shape[2])
+good_lfp_trials_bool = dat.lfp_processing.return_good_lfp_trial_inds(
+    flat_region_lfps)
+good_trial_labels = trial_labels[good_lfp_trials_bool]
 good_lfp_data = dat.lfp_processing.return_good_lfp_trials(
     flat_region_lfps)
 
@@ -75,67 +84,71 @@ this_granger.preprocess_and_check_stationarity()
 # NOTE: It might be that the across trial preprocessing 
 # is messing with single trial analysis
 
-wanted_inds = [2000, 3500]
+#epoch_names = ['pre','early', 'middle', 'late', 'all']
+#epoch_lims = [[1500,2000],[2000,2500],[2500,3000],[3000,3500],[2000, 3500]]
+epoch_starts = np.arange(1500, 3000, 25)
+epoch_ends = epoch_starts + 300
+epoch_lims = list(zip(epoch_starts, epoch_ends))
+epoch_names = epoch_ends #[int(np.mean(x)) for x in epoch_lims]
 input_data = this_granger.preprocessed_data
-input_data = input_data[...,wanted_inds[0]:wanted_inds[1]]
+input_data_epochs = [input_data[...,inds[0]:inds[1]] for inds in epoch_lims]
 # Reshape to (n_trials, n_channels, n_timepoints)
-input_data = np.transpose(input_data, (1,0,2))
+input_data_epochs = [np.transpose(x, (1,0,2)) for x in input_data_epochs]
+#[x.shape for x in input_data_epochs]
 
-# Use BIC of VAR model to determine optimal lag
-max_lag = 100
-
-temp_fit = lambda x: VAR(x.T).fit(maxlags=max_lag, ic='bic').k_ar
-model_orders = parallelize(temp_fit, input_data)
-chosen_model_order = int(np.median(model_orders))
+# # Use BIC of VAR model to determine optimal lag
+# max_lag = 100
+# 
+# temp_fit = lambda x: VAR(x.T).fit(maxlags=max_lag, ic='bic').k_ar
+# model_orders = [parallelize(temp_fit, x) for x in input_data_epochs]
+# chosen_model_order_list = [int(np.median(x)) for x in model_orders]
+# chosen_model_order = int(np.median(chosen_model_order_list))
 
 ############################################################
 temp_gc_full = \
         lambda x: grangercausalitytests(x.T, [chosen_model_order], verbose=False)
 temp_gc_final = lambda x: temp_gc_full(x)[chosen_model_order][0]['params_ftest']
 
-gc_results_out = [parallelize(temp_gc_final, this_dat) \
-        for this_dat in [input_data, input_data[:,::-1]]]
-zipped_results = [list(zip(*x)) for x in gc_results_out]
-cat_results = [[x,y] for x,y in zip(*zipped_results)]
-f_stat, p_val, df_num, k_order = [np.stack(x) for x in cat_results]
+gc_results_out = [[parallelize(temp_gc_final, this_dat) \
+        for this_dat in [x, x[:,::-1]]] \
+        for x in input_data_epochs]
 
-############################################################
-# Plots
-############################################################
+# Convert output to pandas dataframe for easier handling
+direction_names = [x.join(region_names) for x in ['-->','<--']]
 
-# Plot f-statistic of each direction against the other
-# Check which side f-stats lean to
-f_stat_ratio = f_stat[0]/f_stat[1]
+# gc_results_out : [epoch][direction][trial][f_stat, p_val, df_num, k_order]
+gc_results_flat = []
+for epoch_num, epoch in enumerate(gc_results_out):
+    for direction_num, direction in enumerate(epoch):
+        for trial_num, trial in enumerate(direction):
+            gc_results_flat.append(
+                    [epoch_names[epoch_num], direction_names[direction_num], trial_num] + list(trial))
+gc_results_df = pd.DataFrame(gc_results_flat, columns = \
+        ['epoch', 'direction', 'trial', 'f_stat', 'p_val', 'df_num', 'k_order'])
+gc_results_df['taste_num'] = good_trial_labels[gc_results_df['trial']]
+gc_results_df['taste'] = gc_results_df['taste_num'].map(lambda x: taste_names[x])
+# Log transform f-statistic for parametric testing
+gc_results_df['log_f_stat'] = np.log10(gc_results_df['f_stat'])
 
-fig,ax = plt.subplots(1,2)
-ax[0].scatter(*f_stat)
-ax[0].plot([0,np.max(f_stat)], [0,np.max(f_stat)], color='red')
-ax[0].set_aspect('equal')
-ax[1].hist(np.log10(f_stat_ratio), bins = 30)
-ax[1].axvline(np.log10(1), color='red')
-ax[1].axvline(np.log10(f_stat_ratio).mean(),
-              color = 'black', linestyle = '--')
-ax[1].set_title(f'Ration > 1: {np.mean(f_stat_ratio > 1):.2f}')
-plt.show()
+# Test difference in log f-statistic between epochs, direction, and taste
+pg.normality(data=gc_results_df, dv='log_f_stat', group='epoch')
+pg.normality(data=gc_results_df, dv='log_f_stat', group='direction')
+pg.normality(data=gc_results_df, dv='log_f_stat', group='taste')
 
-# Plot log p-value of each direction against the other
-log10_p_val = np.log10(p_val)
-alpha = 0.05
-log10_alpha = np.log10(alpha)
-sig_trials = log10_p_val < log10_alpha
+pg.anova(data=gc_results_df, dv='log_f_stat', 
+         between=['epoch', 'direction', 'taste'])
 
-mean_sig_trials = np.mean(sig_trials, axis=-1)
+# Perform pairwise comparisons between epochs, direction, and taste
+pg.pairwise_ttests(data=gc_results_df, dv='log_f_stat',
+                   between=['epoch', 'direction', 'taste'],
+                   )
 
-plt.scatter(*log10_p_val)
-plt.axhline(log10_alpha, color='red')
-plt.axvline(log10_alpha, color='red')
-plt.show()
+## Plot log f-statistic for each epoch, direction, and taste_names using seaborn
+#sns.catplot(x='epoch', y='log_f_stat', hue='direction',
+#            col='taste', data=gc_results_df, kind='violin')
+#plt.show()
 
-
-plt.scatter(log10_p_val, f_stat, s=5)
-plt.axvline(log10_alpha, color='red')
-plt.xlabel('log10 p value')
-plt.ylabel('F statistic')
-plt.title('Granger Causality' + '\n' + \
-        f'Sig Trial Fraction: {np.mean(sig_trials):.2f}')
+# Plot log f-statistic for each for each taste and direction over epochs as lineplots
+sns.relplot(x='epoch', y='log_f_stat', hue='direction',
+            col='taste', data=gc_results_df, kind='line')
 plt.show()
