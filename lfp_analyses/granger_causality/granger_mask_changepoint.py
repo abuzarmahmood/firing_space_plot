812b47f57b2a60181c19ed3aba03b5cd9299f4e5
Abuzar Mahmood <abuzarmahmood@gmail.com>
2023-05-31 09:31:00 -0400

    Backup

--- lfp_analyses/granger_causality/granger_mask_changepoint.py
+++ lfp_analyses/granger_causality/granger_mask_changepoint.py
@@ -38,6 +38,19 @@ session_count = len(basename_list)
 
 n_string = f'N = {session_count} sessions, {len(animal_count[0])} animals'
 
+# Write out basenames to plot_dir
+name_frame = pd.DataFrame(
+        dict(
+            animal_name = animal_name,
+            session_name = basename_list
+            )
+        )
+name_frame = name_frame.sort_values(by = list(name_frame.columns))
+name_frame.reset_index(drop=True, inplace=True)
+name_frame.to_csv(os.path.join(plot_dir_base, 'session_names.txt'),
+                  sep = '\t', index=False)
+
+
 save_path = '/ancillary_analysis/granger_causality/all'
 names = ['granger_actual',
          'masked_granger',
@@ -85,106 +98,134 @@ mean_mask = np.nanmean(mask_array, axis=0)
 dir_names = ['BLA-->GC', 'GC-->BLA']
 mean_mask = np.stack([mean_mask[...,0,1],mean_mask[...,1,0],], axis=-1).T
 
-# Plot mean mask
-fig, ax = plt.subplots(2,1)
-for this_mask, this_ax in zip(mean_mask, ax):
-    this_ax.imshow(this_mask, aspect='auto', origin='lower',
+# Plot all masks
+plot_mask_array = np.stack(
+        [mask_array[..., 0, 1], mask_array[..., 1, 0]], axis=-1
+        )
+plot_mask_array = np.moveaxis(plot_mask_array, -1, 1)
+
+fig, ax = plt.subplots(*plot_mask_array.shape[:2],
+                       sharex=True, sharey=True,
+                       figsize = (5,15))
+inds = np.array(list(np.ndindex(*plot_mask_array.shape[:2])))
+for this_ind, this_ax in zip(inds, ax.flatten()):
+    this_mask = plot_mask_array[this_ind[0], this_ind[1]]
+    this_ax.imshow(1-this_mask.T, aspect='auto', origin='lower',
                    extent=[time_vec[0], time_vec[-1],
                            freq_vec[0], freq_vec[-1]],
                    cmap='viridis')
-    this_ax.set_ylabel('Frequency (Hz)')
+    this_ax.set_ylabel('Freq (Hz)')
+for i, this_ax in enumerate(ax[0]):
+    this_ax.set_title(dir_names[i])
+for i, this_ax in enumerate(ax[-1]):
     this_ax.set_xlabel('Time (s)')
-    this_ax.set_title(n_string)
+fig.suptitle('Granger Masks')
 plt.tight_layout()
-plt.show()
+fig.savefig(os.path.join(plot_dir_base, 'all_granger_masks.png'))
+plt.close()
+#plt.show()
+
+
+## Plot mean mask
+#fig, ax = plt.subplots(2,1)
+#for this_mask, this_ax in zip(mean_mask, ax):
+#    this_ax.imshow(this_mask, aspect='auto', origin='lower',
+#                   extent=[time_vec[0], time_vec[-1],
+#                           freq_vec[0], freq_vec[-1]],
+#                   cmap='viridis')
+#    this_ax.set_ylabel('Frequency (Hz)')
+#    this_ax.set_xlabel('Time (s)')
+#    this_ax.set_title(n_string)
+#plt.tight_layout()
+#plt.show()
 
 ############################################################
 # Changepoint Detection
 ############################################################
 
-############################################################
-# Using elbo
-############################################################
-
-# Create and fit model
-n_fit = 80000
-n_samples = 20000
-state_range = np.arange(2, 8+1)
-
-best_model_list = []
-model_list_list = []
-elbo_values_list = []
-for data_array in mean_mask:
-    best_model, model_list, elbo_values = \
-        models.find_best_states(
-                data_array, 
-                models.gaussian_changepoint_mean_2d,
-                n_fit,
-                n_samples,
-                min_states = state_range.min(),
-                max_states = state_range.max()
-    )
-    best_model_list.append(best_model)
-    model_list_list.append(model_list)
-    elbo_values_list.append(elbo_values)
-
-# Plot ELBO values and mark best model
-fig, ax = plt.subplots(2,1)
-for this_elbo, this_ax in zip(elbo_values_list, ax):
-    this_ax.plot(state_range, this_elbo)
-    this_ax.axvline(state_range[np.argmin(this_elbo)], color='r',
-                    linestyle='--', label='Best Model')
-    this_ax.legend()
-    this_ax.set_ylabel('ELBO')
-    this_ax.set_xlabel('Number of States')
-    this_ax.set_title(n_string)
-plt.tight_layout()
-plt.show()
-
-best_state_nums = [5, 5]
-
-model_list = []
-trace_list = []
-tau_list = []
-for data_array, this_state_num in zip(mean_mask, best_state_nums):
-    model = models.gaussian_changepoint_mean_2d(data_array, this_state_num)
-    model, approx, mu_stack, sigma_stack, tau_samples, fit_data = \
-            models.advi_fit(model = model, fit = n_fit, samples = n_samples)
-    trace = approx.sample(2000)
-    trace_list.append(trace)
-    model_list.append(model)
-    tau_list.append(tau_samples)
-
-tau_array = np.stack(tau_list, axis=0)
-int_tau = np.round(tau_array).astype(int)
-mode_tau = np.squeeze(stats.mode(int_tau, axis=1))[0]
-
-tau_reshape = np.moveaxis(tau_array, -1, 1)
-
-fig, ax = plt.subplots(len(tau_reshape), 1, sharex=True)
-for i, this_ax in enumerate(ax):
-    this_dat = tau_reshape[i]
-    for vals in this_dat:
-        this_ax.hist(vals, bins=np.linspace(0,mean_mask.shape[-1]))
-plt.show()
-
-# Plot inferred changepoints from best models with mean_mask_bands
-fig, ax = plt.subplots(2,1)
-for i, this_ax in enumerate(ax):
-    this_dat = mean_mask[i]
-    this_tau = mode_tau[i]
-    im = this_ax.imshow(stats.zscore(this_dat,axis=-1), aspect='auto', origin='lower',
-                   extent=[time_vec[0], time_vec[-1],
-                           freq_vec[0], freq_vec[-1]],
-                   )
-    plt.colorbar(im, ax=this_ax)
-    this_ax.set_ylabel('Frequency (Hz)')
-    this_ax.set_xlabel('Time (s)')
-    this_ax.set_title(n_string)
-    for tau_val in this_tau:
-        this_ax.axvline(time_vec[tau_val], color='r', linestyle='--')
-plt.tight_layout()
-plt.show()
+#############################################################
+## Using elbo
+#############################################################
+#
+## Create and fit model
+#n_fit = 80000
+#n_samples = 20000
+#state_range = np.arange(2, 8+1)
+#
+#best_model_list = []
+#model_list_list = []
+#elbo_values_list = []
+#for data_array in mean_mask:
+#    best_model, model_list, elbo_values = \
+#        models.find_best_states(
+#                data_array, 
+#                models.gaussian_changepoint_mean_2d,
+#                n_fit,
+#                n_samples,
+#                min_states = state_range.min(),
+#                max_states = state_range.max()
+#    )
+#    best_model_list.append(best_model)
+#    model_list_list.append(model_list)
+#    elbo_values_list.append(elbo_values)
+#
+## Plot ELBO values and mark best model
+#fig, ax = plt.subplots(2,1)
+#for this_elbo, this_ax in zip(elbo_values_list, ax):
+#    this_ax.plot(state_range, this_elbo)
+#    this_ax.axvline(state_range[np.argmin(this_elbo)], color='r',
+#                    linestyle='--', label='Best Model')
+#    this_ax.legend()
+#    this_ax.set_ylabel('ELBO')
+#    this_ax.set_xlabel('Number of States')
+#    this_ax.set_title(n_string)
+#plt.tight_layout()
+#plt.show()
+#
+#best_state_nums = [5, 5]
+#
+#model_list = []
+#trace_list = []
+#tau_list = []
+#for data_array, this_state_num in zip(mean_mask, best_state_nums):
+#    model = models.gaussian_changepoint_mean_2d(data_array, this_state_num)
+#    model, approx, mu_stack, sigma_stack, tau_samples, fit_data = \
+#            models.advi_fit(model = model, fit = n_fit, samples = n_samples)
+#    trace = approx.sample(2000)
+#    trace_list.append(trace)
+#    model_list.append(model)
+#    tau_list.append(tau_samples)
+#
+#tau_array = np.stack(tau_list, axis=0)
+#int_tau = np.round(tau_array).astype(int)
+#mode_tau = np.squeeze(stats.mode(int_tau, axis=1))[0]
+#
+#tau_reshape = np.moveaxis(tau_array, -1, 1)
+#
+#fig, ax = plt.subplots(len(tau_reshape), 1, sharex=True)
+#for i, this_ax in enumerate(ax):
+#    this_dat = tau_reshape[i]
+#    for vals in this_dat:
+#        this_ax.hist(vals, bins=np.linspace(0,mean_mask.shape[-1]))
+#plt.show()
+#
+## Plot inferred changepoints from best models with mean_mask_bands
+#fig, ax = plt.subplots(2,1)
+#for i, this_ax in enumerate(ax):
+#    this_dat = mean_mask[i]
+#    this_tau = mode_tau[i]
+#    im = this_ax.imshow(stats.zscore(this_dat,axis=-1), aspect='auto', origin='lower',
+#                   extent=[time_vec[0], time_vec[-1],
+#                           freq_vec[0], freq_vec[-1]],
+#                   )
+#    plt.colorbar(im, ax=this_ax)
+#    this_ax.set_ylabel('Frequency (Hz)')
+#    this_ax.set_xlabel('Time (s)')
+#    this_ax.set_title(n_string)
+#    for tau_val in this_tau:
+#        this_ax.axvline(time_vec[tau_val], color='r', linestyle='--')
+#plt.tight_layout()
+#plt.show()
 
 ################################################################################
 # Repeat for average mask averaged over freq bands
@@ -204,22 +245,22 @@ mean_mask_bands = np.stack(mean_mask_bands, axis=1)
 zscored_mean_mask_bands = np.stack([
     stats.zscore(x, axis=-1) for x in mean_mask_bands])
 
-# Plot mean mask bands
-fig, ax = plt.subplots(2,2)
-for i, this_ax in enumerate(ax):
-    this_ax[0].imshow(mean_mask_bands[i], aspect='auto', origin='lower',
-                   extent=[time_vec[0], time_vec[-1],
-                           band_ranges[0][0], band_ranges[-1][1]],
-                   cmap='viridis')
-    this_ax[1].imshow(zscored_mean_mask_bands[i], aspect='auto', origin='lower',
-                   extent=[time_vec[0], time_vec[-1],
-                           band_ranges[0][0], band_ranges[-1][1]],
-                   cmap='viridis')
-    this_ax[0].set_ylabel('Frequency (Hz)')
-    this_ax[0].set_xlabel('Time (s)')
-    this_ax[0].set_title(n_string)
-plt.tight_layout()
-plt.show()
+## Plot mean mask bands
+#fig, ax = plt.subplots(2,2)
+#for i, this_ax in enumerate(ax):
+#    this_ax[0].imshow(mean_mask_bands[i], aspect='auto', origin='lower',
+#                   extent=[time_vec[0], time_vec[-1],
+#                           band_ranges[0][0], band_ranges[-1][1]],
+#                   cmap='viridis')
+#    this_ax[1].imshow(zscored_mean_mask_bands[i], aspect='auto', origin='lower',
+#                   extent=[time_vec[0], time_vec[-1],
+#                           band_ranges[0][0], band_ranges[-1][1]],
+#                   cmap='viridis')
+#    this_ax[0].set_ylabel('Frequency (Hz)')
+#    this_ax[0].set_xlabel('Time (s)')
+#    this_ax[0].set_title(n_string)
+#plt.tight_layout()
+#plt.show()
 
 ############################################################
 # Using Dirichlet Process Prior 
@@ -330,10 +371,11 @@ plt.close(fig)
 #plt.show()
 
 # Plot 5 state changepoint hists using ppc
+wanted_n_states = 5
 ppc_obs_array = np.stack(ppc_list)
 scaled_ppc_tau_samples = time_vec[ppc_tau.astype('int')]
 w_state_counts = np.sum(ppc_w_latent > set_thresh,axis=-1)
-wanted_inds = np.where(w_state_counts == 5)
+wanted_inds = np.where(w_state_counts == wanted_n_states)
 wanted_tau = [this_scaled_tau[wanted_inds[1][wanted_inds[0]==i]] \
         for i, this_scaled_tau in enumerate(scaled_ppc_tau_samples)]
 wanted_obs = [this_ppc_obs[wanted_inds[1][wanted_inds[0]==i]] \
@@ -346,7 +388,7 @@ for i in range(ax.shape[0]):
     ax[0,i].set_xlabel('Time (s)')
     ax[0,i].set_ylabel('Count')
     ax[0,i].set_title(dir_names[i])
-    ax[1,i].imshow(mean_wanted_obs[i], aspect='auto', origin='lower',
+    ax[1,i].imshow(zscored_mean_mask_bands[i], aspect='auto', origin='lower',
                    extent=[time_vec[0], time_vec[-1],
                            freq_vec[0], freq_vec[-1]],
                    interpolation='none')
@@ -359,11 +401,37 @@ peaks = [-0.07, 0.23, 0.73, 0.93]
 for this_ax in ax.flatten():
     for this_peak in peaks:
         this_ax.axvline(this_peak, color = 'red', linestyle = '--')
-fig.suptitle('Tau Distribution, 5 State')
+fig.suptitle('Tau Distribution, 5 State\n' + str(peaks) )
 plt.tight_layout()
-fig.savefig(os.path.join(plot_dir_base, 'granger_mask_changepoints_dirichlet_5state_ppc.png'),
+fig.savefig(os.path.join(plot_dir_base, 'granger_mask_changepoints_dirichlet_5state.png'),
             dpi = 300)
 plt.close(fig)
+
+# Plot per transition histograms for each tau
+fig, ax = plt.subplots(10,2, sharex=True)
+for i in range(ax.shape[0]):
+    for j in range(ax.shape[1]):
+        this_ax = ax[i,j]
+        this_dat = wanted_tau[j][:,i]
+        this_ax.hist(this_dat, bins = time_vec, color = 'grey')
+plt.show()
+
+# Plot pairs of state changepoint times
+import itertools as it
+inds = list(it.combinations(np.arange(wanted_n_states),2))
+inds = [x for x in inds if np.diff(x) == 1]
+fig, ax = plt.subplots(len(inds),2, sharex=True)
+for i, this_ind in enumerate(inds):
+    for j in range(ax.shape[1]):
+        ax[i,j].scatter(
+                wanted_tau[j][:,this_ind[0]].flatten(), 
+                wanted_tau[j][:,this_ind[1]].flatten(),
+                        color = 'grey', s = 1)
+        ax[i,j].set_xlabel('Time (s)')
+        ax[i,j].set_ylabel('Time (s)')
+        ax[i,j].set_title(f'State {this_ind[j]} to {this_ind[j]+1}')
+plt.tight_layout()
+plt.show()
 
 ## Tau raster plot
 #tau_raster = np.zeros((len(dir_names), tau_samples.shape[1], len(time_vec)))
