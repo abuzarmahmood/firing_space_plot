2eed8b5b2c9d5b2ea07141c9a1333325aae162c6
Abuzar Mahmood <abuzarmahmood@gmail.com>
2025-01-28 14:30:12 -0500

    feat(classifier): integrate SVM for labeling and update feature handling
    - Changed the target variable from 'updated_codes' to 'event_codes'.
    - Implemented SVM with RBF kernel for predictions in the NCA space.
    - Loaded feature names from an external file and incorporated them.
    - Modified plots to use SVM predictions alongside true labels.
    - Updated the file saving format from SVG to PNG for better compatibility.
    - Added plots to compare original and SVM-predicted labels based on segment traces and feature distributions.

--- emg_analysis/mouth_movement_clustering/src/classifier_pipeline/analyses/prec_recall_comparison.py
+++ emg_analysis/mouth_movement_clustering/src/classifier_pipeline/analyses/prec_recall_comparison.py
@@ -47,7 +47,12 @@ all_pred_frame = pd.read_pickle(os.path.join(artifact_dir, 'all_datasets_emg_pre
 ##############################
 feature_array = np.stack(scored_df['features'].values)
 X = feature_array.copy()
-y = scored_df['updated_codes'].values
+y = scored_df['event_codes'].values
+
+# Load feature names
+feature_names_path = os.path.join(artifact_dir, 'all_datasets_feature_names.txt')
+with open(feature_names_path, 'r') as f:
+    feature_names = f.read().split('\n')
 
 # Do not rescale as classifier was trained on unscaled data
 # scaler = StandardScaler()
@@ -65,7 +70,7 @@ X_transform_plot = X_transform.copy()
 # X_transform_plot = X_transform + np.random.normal(0, sd, X_transform.shape)
 
 # Count each 'updated_event_type'
-event_counts = scored_df['updated_event_type'].value_counts()
+event_counts = scored_df['event_type'].value_counts()
 
 ############################################################
 # Perform predictions 
@@ -73,6 +78,8 @@ event_counts = scored_df['updated_event_type'].value_counts()
 event_code_dict_path = os.path.join(artifact_dir, 'event_code_dict.json')
 with open(event_code_dict_path, 'r') as f:
     event_code_dict = json.load(f)
+# event_code_dict = scored_df.groupby('updated_codes')['updated_event_type'].unique()
+# event_code_dict = {k: v[0] for k, v in event_code_dict.items()}
 
 inv_event_code_dict = {v: k for k, v in event_code_dict.items()}
 
@@ -86,7 +93,7 @@ xgb_pred_names = [inv_event_code_dict[x] for x in xgb_pred]
 ##############################
 fig, ax = plt.subplots(1,3, sharex=True, sharey=True, figsize=(15,5))
 for i, this_y in enumerate(np.unique(y)):
-    y_name = scored_df.loc[scored_df['updated_codes'] == this_y, 'updated_event_type'].values[0]
+    y_name = scored_df.loc[scored_df['event_codes'] == this_y, 'event_type'].values[0]
     y_count = event_counts[y_name]
     if y_name == 'gape':
         zorder = 10
@@ -125,9 +132,112 @@ ax[2].legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), shadow=True, ncol=
 plt.suptitle('Neighborhood Components Analysis')
 plt.tight_layout()
 # plt.show()
-fig.savefig(os.path.join(plot_dir, 'jl_comparison_nca.svg'),
+fig.savefig(os.path.join(plot_dir, 'jl_comparison_nca.png'),
             bbox_inches='tight') 
 plt.close(fig)
+
+##############################
+# Relabel to have more defined bounds in 2D NCA space
+# Train k-nearest neighbors on NCA space
+
+# from sklearn.neighbors import KNeighborsClassifier
+# 
+# n_neighbors = 10
+# knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+# knn.fit(X_transform, y)
+# 
+# # Predict on NCA space
+# knn_pred = knn.predict(X_transform)
+# knn_pred_names = [inv_event_code_dict[x] for x in knn_pred]
+
+# Use SVM with RBF kernel
+from sklearn.svm import SVC
+svm = SVC(kernel='rbf')
+svm.fit(X_transform, y)
+svm_pred = svm.predict(X_transform)
+
+fig, ax = plt.subplots(1,2, sharex=True, sharey=True, figsize=(10,5))
+for i, this_y in enumerate(np.unique(y)):
+    ax[0].scatter(*list(X_transform_plot[y == this_y].T),
+                alpha=0.3,
+                  label=f'{this_y}')
+    # ax[1].scatter(*list(X_transform_plot[knn_pred == this_y].T),
+    ax[1].scatter(*list(X_transform_plot[svm_pred == this_y].T),
+                alpha=0.3,
+                  label=f'{this_y}')
+ax[0].set_title('True Labels')
+ax[0].legend()
+ax[1].set_title('SVM Predictions')
+ax[1].legend()
+plt.suptitle('Neighborhood Components Analysis')
+plt.tight_layout()
+fig.savefig(os.path.join(plot_dir, 'svm_relabelling.png'),
+            bbox_inches='tight')
+plt.close(fig)
+
+# Plot traces with both sets of labels
+segments = scored_df['segment_raw'].values
+norm_features = scored_df['features'].values
+amp_ind = [feature_names.index(x) for x in feature_names if 'amp' in x][0]
+norm_amp = np.stack([x[amp_ind] for x in norm_features])
+norm_amp = norm_amp - np.min(norm_amp)
+norm_amp = norm_amp/np.max(norm_amp)
+scaled_segments = [(x/np.max(x))*this_amp for x, this_amp in zip(segments, norm_amp)]
+freq_feature_ind = [feature_names.index(x) for x in feature_names if 'freq' in x][0]
+freq_vals = np.stack(scored_df.raw_features.values)[:, freq_feature_ind]
+
+fig, ax = plt.subplots(len(np.unique(y)), 2,
+                       sharex=True, sharey=True, figsize=(10, 5*len(np.unique(y)))) 
+for segment_ind, this_segment in enumerate(scaled_segments):
+    og_label = y[segment_ind]
+    svm_label = svm_pred[segment_ind]
+    og_row_ind = np.where(np.unique(y) == og_label)[0][0]
+    svm_row_ind = np.where(np.unique(y) == svm_label)[0][0]
+    ax[og_row_ind, 0].plot(this_segment, alpha=0.1, c='k')
+    ax[svm_row_ind, 1].plot(this_segment, alpha=0.1, c='k')
+for i, this_y in enumerate(np.unique(y)):
+    class_name = scored_df.loc[scored_df['event_codes'] == this_y, 'event_type'].values[0]
+    ax[i, 0].set_ylabel(class_name)
+ax[0, 0].set_title('True Labels')
+ax[0, 1].set_title('Modified Labels')
+plt.suptitle('Segment Traces')
+plt.tight_layout()
+fig.savefig(os.path.join(plot_dir, 'relabelled_segment_traces.png'),
+            bbox_inches='tight')
+plt.close(fig)
+
+# Also plot distributions of heights for og and svm labels
+fig, ax = plt.subplots(
+    len(np.unique(y)), 2,
+    sharex='col', sharey=False,
+    figsize=(10, 5*len(np.unique(y)))
+    )
+bins = np.linspace(0, 1, 50)
+freq_bins = np.linspace(5, 15, 50)
+for i, this_y in enumerate(np.unique(y)):
+    og_label = y == this_y
+    svm_label = svm_pred == this_y
+    ax[i,0].hist(norm_amp[og_label], alpha=0.5, 
+                 label=f'Orig labels, count={np.sum(og_label)}', bins=bins)
+    ax[i,0].hist(norm_amp[svm_label], alpha=0.5, 
+                 label=f'New labels, count={np.sum(svm_label)}', bins=bins)
+    ax[i,0].legend()
+    class_name = scored_df.loc[scored_df['event_codes'] == this_y, 'event_type'].values[0]
+    ax[i,0].set_ylabel(class_name)
+    # Also plot frequency
+    ax[i,1].hist(freq_vals[og_label], alpha=0.5, label='Orig Labels', bins=freq_bins)
+    ax[i,1].hist(freq_vals[svm_label], alpha=0.5, label='New Labels', bins=freq_bins)
+    ax[i,1].legend()
+ax[0,0].set_title('Amp Distributions')
+ax[0,1].set_title('Freq Distributions')
+ax[-1,0].set_xlabel('Normalized Amplitude')
+ax[-1,1].set_xlabel('Frequency (Hz)')
+plt.suptitle('Orig vs Relabelled Feature Distributions')
+# plt.tight_layout()
+fig.savefig(os.path.join(plot_dir, 'relabelled_height_distributions.png'),
+            bbox_inches='tight')
+plt.close(fig)
+
 
 ##############################
 # Calculate precision, recall, and f1 for
